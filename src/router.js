865a71de3c57e06d90c6e150bbd161d66d0c36ca
David Sato <david.sato64@gmail.com>
2017-10-09 15:48:25 +0200

    restructured router.js to basic format

--- src/router.js
+++ src/router.js
@@ -3,18 +3,9 @@ import PropTypes from 'prop-types'
 import {
   Router,
   Route,
-  IndexRedirect,
 } from 'react-router'
 
 import App from 'containers/App'
-import MarketListPage from 'containers/MarketListPage'
-import MarketCreateWizardPage from 'containers/MarketCreateWizardPage'
-import MarketDetailPage from 'containers/MarketDetailPage'
-import TransactionsPage from 'containers/TransactionsPage'
-import AccountPage from 'containers/AccountPage'
-import SettingsPage from 'containers/SettingsPage'
-import DashboardPage from 'containers/DashboardPage'
-import MarketCreateReviewPage from 'containers/MarketCreateReviewPage'
 
 class AppRouter extends Component {
   static propTypes = {
@@ -25,27 +16,9 @@ class AppRouter extends Component {
     const { history } = this.props
 
     return (
+        // TODO - ask about Math.random()
       <Router key={Math.random()} history={history}>
-        <Route path="/" component={App}>
-          <IndexRedirect to="dashboard" />
-          <Route path="dashboard" component={DashboardPage} />
-          <Route path="transactions" component={TransactionsPage} />
-          <Route path="account" component={AccountPage} />
-          <Route path="settings" component={SettingsPage} />
-          <Route path="markets">
-            <IndexRedirect to="list" />
-            <Route path="new" component={MarketCreateWizardPage} />
-            <Route path="review" component={MarketCreateReviewPage} />
-            <Route path="list" component={MarketListPage} />
-            <Route path=":id" component={MarketDetailPage}>
-              <Route path=":view">
-                <Route path=":shareId">
-                  <Route path="sell" component={MarketDetailPage} />
-                </Route>
-              </Route>
-            </Route>
-          </Route>
-        </Route>
+        <Route path="/" component={App} />
       </Router>
     )
   }
