438a10c05b4f2ac2289024afd6b07e2c32de657d
Velenir <Velenir@users.noreply.github.com>
2017-10-17 13:43:57 +0300

    added tslint

--- src/router.tsx
+++ src/router.tsx
@@ -1,32 +1,32 @@
 import React from 'react'
 import { ConnectedRouter } from 'connected-react-router'
 import {
-    Route,
-    NavLink,
+  Route,
+  NavLink,
 } from 'react-router-dom'
 import { History } from 'history'
 
 // import App from 'containers/App'
 
 const TempApp = () => (
-    <div>
-        <h2>Hello World!</h2>
-    </div>
+  <div>
+    <h2>Hello World!</h2>
+  </div>
 )
 
 interface AppRouterProps {
-    history: History
+  history: History
 }
 
 const AppRouter: React.SFC<AppRouterProps> = ({ history }) => (
-    <ConnectedRouter history={history}>
-        <div>
-            <NavLink to="/">Home</NavLink>{' '}
-            <NavLink to="/someroute">Some Route</NavLink>
-            <Route exact path="/" component={TempApp} />
-            <Route path="/someroute" render={() => <h3>Some Route</h3>} />
-        </div>
-    </ConnectedRouter>
+  <ConnectedRouter history={history}>
+    <div>
+      <NavLink to="/">Home</NavLink>{' '}
+      <NavLink to="/someroute">Some Route</NavLink>
+      <Route exact path="/" component={TempApp} />
+      <Route path="/someroute" render={() => <h3>Some Route</h3>} />
+    </div>
+  </ConnectedRouter>
 )
 
 
