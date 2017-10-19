899df4966792baa7f4905f0de90a9449e4052907
Velenir <Velenir@users.noreply.github.com>
2017-10-19 17:16:20 +0300

    rewrote injectedWeb3 base class to fit with new Metamask implementation

--- src/integrations/injectedWeb3.ts
+++ src/integrations/injectedWeb3.ts
@@ -2,70 +2,101 @@ import { ETHEREUM_NETWORKS } from './constants'
 import { weiToEth } from 'utils/helpers'
 // const autobind = require('autobind-decorator')
 
-class InjectedWeb3 {
-  runProviderUpdate: any
-  runProviderRegister: any
-  watcherInterval: any
-  web3: any
-  account: any
-  network: any
-  balance: any
-  walletEnabled: any
+const WATCHER_INTERVAL = 1000
 
-  constructor() {
-    this.watcher = this.watcher.bind(this)
-    
-    this.watcherInterval = setInterval(this.watcher, 1000)
+const networkById = {
+  1: ETHEREUM_NETWORKS.MAIN,
+  2: ETHEREUM_NETWORKS.MORDEN,
+  3: ETHEREUM_NETWORKS.ROPSTEN,
+  4: ETHEREUM_NETWORKS.RINKEBY,
+  42: ETHEREUM_NETWORKS.KOVAN,
+}
+
+export default class InjectedParent {
+  private watcherInterval: number
+  protected account: any
+  protected network: any
+  protected balance: any
+  protected walletEnabled: boolean = false
+  protected web3: any
+  protected providerName: string
+
+  protected static registerProvider: Function
+  protected static updateProvider: Function
+
+  protected constructor() {
+
+    this.watcherInterval = setInterval(this.watcher.bind(this), WATCHER_INTERVAL)
   }
 
-  async initialize(opts?: any) {
-    this.runProviderUpdate = typeof opts.runProviderUpdate === 'function' ?
-      opts.runProviderUpdate : this.runProviderUpdate
-    this.runProviderRegister = typeof opts.runProviderRegister === 'function' ?
-      opts.runProviderRegister : this.runProviderRegister
+  static initialize({ registerProvider, updateProvider }: { [key: string]: Function }) {
+    InjectedParent.registerProvider = registerProvider
+    InjectedParent.updateProvider = updateProvider
   }
 
-  async getNetwork() {
+  /**
+   * Periodic updater to get all relevant information from this provider
+   * @async
+   */
+  protected async watcher() {
+    try {
+      const [currentAccount, currentNetwork] = await Promise.all([
+        this.getAccount(),
+        this.getNetwork(),
+      ])
+
+      const providerState: { [key: string]: any } = {}
+
+      if (this.account !== currentAccount) {
+        this.account = providerState.account = currentAccount
+      }
+
+      if (this.network !== currentNetwork) {
+        this.network = providerState.network = currentNetwork
+      }
+
+      // depends on this.account set inside this.getAccount
+      // TODO: rewrite in a more independent way
+      const currentBalance = await this.getBalance()
+
+      if (this.balance !== currentBalance) {
+        this.balance = providerState.balance = currentBalance
+      }
+
+      if (!this.walletEnabled && currentAccount) {
+        this.walletEnabled = providerState.available = true
+      }
+
+      console.log('WATCHER state', providerState)
+
+      // if providerState is filled with new data
+      if (Object.keys(providerState).length) {
+        InjectedParent.updateProvider(this.providerName, providerState)
+      }
+    } catch (err) {
+      if (this.walletEnabled) {
+        this.walletEnabled = false
+        return InjectedParent.updateProvider(this.providerName, { available: false })
+      }
+    }
+  }
+
+  protected async getNetwork() {
     return new Promise((resolve, reject) => {
-      this.web3.version.getNetwork((err: any, netId: any) => {
+      this.web3.version.getNetwork((err: Error, netId: any) => {
         if (err) {
-          reject(err)
-        } else {
-          switch (netId) {
-            case '1': {
-              resolve(ETHEREUM_NETWORKS.MAIN)
-              break
-            }
-            case '2': {
-              resolve(ETHEREUM_NETWORKS.MORDEN)
-              break
-            }
-            case '3': {
-              resolve(ETHEREUM_NETWORKS.ROPSTEN)
-              break
-            }
-            case '4': {
-              resolve(ETHEREUM_NETWORKS.RINKEBY)
-              break
-            }
-            case '42': {
-              resolve(ETHEREUM_NETWORKS.KOVAN)
-              break
-            }
-            default: {
-              resolve(ETHEREUM_NETWORKS.UNKNOWN)
-              break
-            }
-          }
+          return reject(err)
         }
+
+        return resolve(networkById[netId] || ETHEREUM_NETWORKS.UNKNOWN)
       })
     })
   }
 
-  async getAccount() {
+  protected async getAccount() {
     return new Promise((resolve, reject) => {
       this.web3.eth.getAccounts(
-        (e: any, accounts: any) => {
+        (e: Error, accounts: any) => {
           if (e) {
             reject(e)
           }
@@ -76,47 +107,11 @@ class InjectedWeb3 {
   }
 
   /**
-   * Periodic updater to get all relevant information from this provider
-   * @async
-   */
-  async watcher() {
-    try {
-      const currentAccount = await this.getAccount()
-      if (this.account !== currentAccount) {
-        this.account = currentAccount
-        await this.runProviderUpdate(this, { account: this.account })
-      }
-
-      const currentNetwork = await this.getNetwork()
-      if (this.network !== currentNetwork) {
-        this.network = currentNetwork
-        await this.runProviderUpdate(this, { network: this.network })
-      }
-
-      const currentBalance = await this.getBalance()
-      if (this.balance !== currentBalance) {
-        this.balance = currentBalance
-        await this.runProviderUpdate(this, { balance: this.balance })
-      }
-
-      if (!this.walletEnabled && currentAccount) {
-        this.walletEnabled = true
-        await this.runProviderUpdate(this, { available: true })
-      }
-    } catch (err) {
-      if (this.walletEnabled) {
-        this.walletEnabled = false
-        await this.runProviderUpdate(this, { available: false })
-      }
-    }
-  }
-
-  /**
    * Returns the balance for the current default account in Wei
    * @async
    * @returns {Promise<string>} - Accountbalance in WEI for current account
    */
-  async getBalance() {
+  protected async getBalance() {
     return new Promise((resolve, reject) => {
       if (this.account) {
         this.web3.eth.getBalance(
@@ -130,5 +125,3 @@ class InjectedWeb3 {
   }
 
 }
-
-export default InjectedWeb3
