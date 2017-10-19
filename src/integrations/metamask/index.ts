376e4367b9f5b9e564a92ef2b1a974226b7c4a02
Velenir <Velenir@users.noreply.github.com>
2017-10-19 17:17:47 +0300

    rewrote Metamask class to be created dynamically on initialization instead of statically as a singleton

--- src/integrations/metamask/index.ts
+++ src/integrations/metamask/index.ts
@@ -2,12 +2,7 @@ import { WALLET_PROVIDER } from 'integrations/constants'
 import InjectedWeb3 from 'integrations/injectedWeb3'
 const Web3 = require('web3')
 
-class Metamask extends InjectedWeb3 {
-  account: Object
-  network: Object
-  walletEnabled: boolean
-  web3: any
-
+export default class Metamask extends InjectedWeb3 {
   static providerName = WALLET_PROVIDER.METAMASK
 
   /**
@@ -16,22 +11,35 @@ class Metamask extends InjectedWeb3 {
    */
   static providerPriority = 90
 
-  /**
-   * Tries to initialize and enable the current provider
-   * @param {object} opts - Integration Options
-   * @param {function} opts.runProviderUpdate - Function to run when this provider updates
-   * @param {function} opts.runProviderRegister - Function to run when this provider registers
-   */
-  async initialize(opts: Object) {
-    //run InjectedWeb3's initialise
-    super.initialize(opts)
-    this.runProviderRegister(this, { priority: Metamask.providerPriority })
+  constructor(...props: any[]) {
+    super(...props)
 
-    this.walletEnabled = false
-    
+    this.providerName = Metamask.providerName
+  }
+
+  static async initialize(runProviderFuncs: { [key: string]: Function }) {
+    // const instance = super.initialize(runProviderRegister, runProviderUpdate) as Metamask
+
+    const { registerProvider, updateProvider } = runProviderFuncs
+
+    super.initialize(runProviderFuncs)
+
+    registerProvider(Metamask.providerName, { priority: Metamask.providerPriority })
+
+    // creates instance of Metamask class
+    const instance = new this
+
+    // Check if a Provider injected
+    const providerState = await instance.getInitialState()
+    return updateProvider(Metamask.providerName, providerState)
+  }
+
+  public async getInitialState() {
     // Check if a Provider injected
     try {
-      if (typeof window.web3 !== 'undefined' && window.web3.currentProvider.constructor.name === 'MetamaskInpageProvider') {
+      if (typeof window.web3 !== 'undefined' &&
+        window.web3.currentProvider.constructor.name === 'MetamaskInpageProvider'
+      ) {
         this.web3 = new Web3(window.web3.currentProvider)
         this.walletEnabled = true
       } else {
@@ -39,20 +47,20 @@ class Metamask extends InjectedWeb3 {
       }
     } catch (e) {
       this.walletEnabled = false
-    }  
-
-    if (this.walletEnabled) {
-      this.network = await this.getNetwork()
-      this.account = await this.getAccount()
     }
 
-    return this.runProviderUpdate({
+    if (this.walletEnabled) {
+      [this.network, this.account] = await Promise.all([
+        this.getNetwork(),
+        this.getAccount(),
+      ])
+    }
+
+    return {
       available: this.walletEnabled && this.account !== null,
       network: this.network,
       account: this.account,
       // balance: this.balance,
-    })
+    }
   }
 }
-
-export default new Metamask()
