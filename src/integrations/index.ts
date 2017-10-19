a82130d7879d2d2f2f70b2731f8c81e3a00901d9
Velenir <Velenir@users.noreply.github.com>
2017-10-19 17:19:24 +0300

    temporarily disabled Parity and Remote providers pending their refactor

--- src/integrations/index.ts
+++ src/integrations/index.ts
@@ -1,5 +1,6 @@
 import Metamask from './metamask'
-import Parity from './parity'
-import Remote from './remote'
-
-export { Metamask, Parity, Remote }
+// import Parity from './parity'
+// import Remote from './remote'
+// TODO: refactor Parity and Remote to work with new InjectWeb3
+// export { Metamask, Parity, Remote }
+export { Metamask }
