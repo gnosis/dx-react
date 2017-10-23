534922f878ad439b2f294f844dc1e49b522d0b66
Velenir <Velenir@users.noreply.github.com>
2017-10-23 12:38:49 +0300

    added some storybook addons and decorators

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,20 @@
+import { addDecorator, configure, setAddon } from '@storybook/react'
+import { withKnobs } from '@storybook/addon-knobs'
+import JSXAddon from 'storybook-addon-jsx'
+
+import '../src/styles/global.scss'
+
+window.regeneratorRuntime = require('babel-runtime/regenerator')
+
 const req = require.context('../src/components', true, /\.stories\.tsx$/)
 
-window.regeneratorRuntime = require('babel-runtime/regenerator');
+
+addDecorator(withKnobs)
+setAddon(JSXAddon)
 
 function loadStories() {
+  require('../stories/index.tsx')
   req.keys().forEach((filename) => req(filename))
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
