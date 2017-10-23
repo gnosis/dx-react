45efdb538108c7f8d4ec805f9acd6c025cbc685b
Velenir <Velenir@users.noreply.github.com>
2017-10-23 21:33:03 +0300

    fixed indentation

--- src/components/TokenOverlay/TokenOverlay.stories.tsx
+++ src/components/TokenOverlay/TokenOverlay.stories.tsx
@@ -17,52 +17,52 @@ import TokenOverlay from './index'
 
 
 const code2tokenMap = {
-    ETH: 'ETHER',
-    GNO: 'GNOSIS',
-    REP: 'AUGUR',
-    '1ST': 'FIRST BLOOD',
-    OMG: 'OMISEGO',
-    GNT: 'GOLEM',
+  ETH: 'ETHER',
+  GNO: 'GNOSIS',
+  REP: 'AUGUR',
+  '1ST': 'FIRST BLOOD',
+  OMG: 'OMISEGO',
+  GNT: 'GOLEM',
 }
 
 const codeList = Object.keys(code2tokenMap)
 
 // TODO: get token balance from redux
 const tokenBalances = {
-    ETH: Math.random() * 5,
-    GNO: Math.random() * 5,
-    REP: Math.random() * 5,
-    '1ST': Math.random() * 5,
-    OMG: Math.random() * 5,
-    GNT: Math.random() * 5,
+  ETH: Math.random() * 5,
+  GNO: Math.random() * 5,
+  REP: Math.random() * 5,
+  '1ST': Math.random() * 5,
+  OMG: Math.random() * 5,
+  GNT: Math.random() * 5,
 }
 
 
 const CenterDecor = (story: Function) => (
-    <div
-        style={{
-            display: 'flex',
-            height: '100vh',
-            justifyContent: 'center',
-            alignItems: 'center',
-        }}
-    >
-        <div style={{
-            padding: 20,
-            backgroundColor: 'white',
-            width: 550,
-            height: 500,
-            position: 'relative',
-        }}>
-            {story()}
-        </div>
+  <div
+    style={{
+      display: 'flex',
+      height: '100vh',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <div style={{
+      padding: 20,
+      backgroundColor: 'white',
+      width: 550,
+      height: 500,
+      position: 'relative',
+    }}>
+      {story()}
     </div>
+  </div>
 )
 
 storiesOf('TokenOverlay', module)
-    .addDecorator(CenterDecor)
-    .addWithJSX('open', () => <TokenOverlay
-        closeOverlay={action('CLOSE OVERLAY')}
-        tokenCodeList={array('tokenCodeList', codeList)}
-        tokenBalances={tokenBalances}
-    />)
+  .addDecorator(CenterDecor)
+  .addWithJSX('open', () => <TokenOverlay
+    closeOverlay={action('CLOSE OVERLAY')}
+    tokenCodeList={array('tokenCodeList', codeList)}
+    tokenBalances={tokenBalances}
+  />)
