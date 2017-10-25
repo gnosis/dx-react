33e3f61b019d2b7cd8abbc45b2e1ed5ead9edd2c
Velenir <Velenir@users.noreply.github.com>
2017-10-25 15:26:24 +0300

    introduced globals

---
+++ src/globals/index.ts
@@ -0,0 +1,10 @@
+export const code2tokenMap = {
+  ETH: 'ETHER',
+  GNO: 'GNOSIS',
+  REP: 'AUGUR',
+  '1ST': 'FIRST BLOOD',
+  OMG: 'OMISEGO',
+  GNT: 'GOLEM',
+}
+
+export type TokenCode = keyof typeof code2tokenMap
