09ad5f0d6b43015b8993f5df93b3f6f202ad7423
David Sato <david.sato64@gmail.com>
2017-10-26 11:55:19 +0200

    changed stories/Header name to better reflect it's status w/the separated components inserted - removed unnecessary path sections in import files

--- stories/Header.tsx
+++ stories/Header.tsx
@@ -2,15 +2,15 @@ import * as React from 'react'
 
 const { storiesOf } = require('@storybook/react')
 
-import createStoreWithHistory from '../src/store.js'
+import createStoreWithHistory from 'store.js'
 import createHistory from 'history/createBrowserHistory'
 const history = createHistory()
 const store = createStoreWithHistory(history)
 import { Provider } from 'react-redux'
 
-import Header from '../src/components/Header'
+import Header from 'components/Header'
 
-storiesOf('Header', module)
+storiesOf('Header [v2]', module)
   .addDecorator((story: any) => <Provider store={store as any}>{story()}</Provider>)
-  .add('Dumb Header', () => <Header />)
+  .add('Header', () => <Header />)
   
