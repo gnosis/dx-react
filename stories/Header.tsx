8d3633b761d04cb0298ec36bd773436a6355dbd7
David Sato <david.sato64@gmail.com>
2017-10-26 15:58:01 +0200

    fixed SCSS error in _navbar where tbody was being highlighted on hover instead of <tr> || removed unused import in Header || added random auctions for stories/MenuAuction || linting

--- stories/Header.tsx
+++ stories/Header.tsx
@@ -13,4 +13,3 @@ import Header from 'components/Header'
 storiesOf('Header [v2]', module)
   .addDecorator((story: any) => <Provider store={store as any}>{story()}</Provider>)
   .add('Header', () => <Header />)
-  
--- stories/MenuAuction.tsx
+++ stories/MenuAuction.tsx
@@ -1,53 +1,72 @@
 import * as React from 'react'
 
 import { storiesOf } from '@storybook/react'
-import { array, text } from '@storybook/addon-knobs'
-// import { decorateAction } from '@storybook/addon-actions'
+import { object, text } from '@storybook/addon-knobs'
 
 import MenuAuctions from 'components/MenuAuctions'
 
-const CenterDecor = (story: Function) => (
-  <div
-    style={{
-      display: 'flex',
-      height: '100vh',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
-  >
-    <div style={{
-      padding: 20,
-      backgroundColor: 'transparent',
-    }}>
+const tokenArr = [
+  'GNO',
+  'ETH',
+  '1ST',
+  'BIT',
+  'EQO',
+  '666',
+  'COIN',
+  'YO',
+  'GHTO',
+  'POLKA',
+  'AUGR',
+]
+
+const getRandomInt = (min: number, max: number) => {
+  const mn = Math.ceil(min)
+  const mx = Math.floor(max)
+  return Math.floor(Math.random() * (mx - mn)) + mn
+}
+
+// Create Fn that returns Array of length(x) w/Auction props (a,b,c,d,e)
+const auctionFactory = (amt: number) => {
+  const auctionsToShow = []
+  
+  for (let i = 0; i <= amt; i = i + 1) {
+    auctionsToShow.push({
+      id: +(Math.random() * 5).toFixed(3).toString(),
+      sellToken: tokenArr[getRandomInt(0, tokenArr.length)],
+      buyToken: tokenArr[getRandomInt(0, tokenArr.length)],
+      buyPrice: +(Math.random() * 5).toFixed(4),
+      claim: getRandomInt(0,4) < 2 ? false : true,
+    })
+  }
+  return auctionsToShow
+}
+
+const TopCenterDecor = (story: Function) => (
+  <header>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
       {story()}
     </div>
-  </div>
+  </header>  
 )
 
 const constructKnobs = (
   name = 'YOUR AUCTIONS',
-  auctionsArr: Array<any>,
-  separator: any
+  auctionsArr: [any] | any,
 ) => ({
   name: text('title', name),
-  ongoingAuctions: array('Ongoing Auctions', auctionsArr, separator)
+  ongoingAuctions: auctionsArr.map((item: Object, i: number) => object(`Ongoing Auctions ${i}`, item)),
 })
 
-const ongoingAuctions = [
-  {
-    id: '123',
-    sellToken: 'ETH',
-    buyToken: 'GNO',
-    buyPrice: 117,
-    claim: true
-  },
-]
-
 storiesOf(`MenuAuctions`, module)
-  .addDecorator(CenterDecor)
+  .addDecorator(TopCenterDecor)
   .addWithJSX('MenuAuctionsComponent', () => 
     <MenuAuctions
-      {...constructKnobs('YOUR AUCTIONS', ongoingAuctions, ',') }
-    />
+      {...constructKnobs('YOUR AUCTIONS', auctionFactory(getRandomInt(1, 6))) }
+    />,
   )
 
