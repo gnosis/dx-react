ecea3cfbe55b2eddcf6b7bcb93b961cd6607ed03
Velenir <Velenir@users.noreply.github.com>
2017-10-30 11:53:32 +0300

    changed stories to use global types

--- stories/TokenItem.tsx
+++ stories/TokenItem.tsx
@@ -1,21 +1,15 @@
 import * as React from 'react'
 
-import { storiesOf } from '@storybook/react'
+import { storiesOf, StoryDecorator } from '@storybook/react'
 import { text, number } from '@storybook/addon-knobs'
 import { decorateAction } from '@storybook/addon-actions'
 
-import TokenItem from 'components/TokenItem'
+import TokenItem, { TokenItemProps } from 'components/TokenItem'
+import { code2tokenMap, codeList } from 'globals'
+import { TokenMod, Balance, TokenCode, TokenName } from 'types'
 
-const code2tokenMap = {
-  ETH: 'ETHER',
-  GNO: 'GNOSIS',
-  REP: 'AUGUR',
-  '1ST': 'FIRST BLOOD',
-  OMG: 'OMISEGO',
-  GNT: 'GOLEM',
-}
 
-const CenterDecor = (story: Function) => (
+const CenterDecor: StoryDecorator = story => (
   <div
     style={{
       display: 'flex',
@@ -33,28 +27,28 @@ const CenterDecor = (story: Function) => (
   </div>
 )
 
-const constructKnobs = (name: string, code: string, balance: number, mod?: string) => ({
+const constructKnobs = (name: TokenName, code: TokenCode, balance: Balance, mod?: TokenMod) => ({
   name: text('name', name),
   code: text('code', code),
-  balance: number('balance', balance, {
+  balance: number('balance', +balance, {
     range: true,
     min: 0,
     max: 100,
     step: 0.00000001,
-  }),
+  }).toString(),
   mod: text('mod', mod),
-})
+}) as TokenItemProps
 
 const stringifyAction = decorateAction([
   args => [JSON.stringify(args[0])],
 ])
 
 
-for (const code of Object.keys(code2tokenMap)) {
+for (const code of codeList) {
   const name = code2tokenMap[code]
 
-  const r1 = +(Math.random() * 5).toFixed(9)
-  const r2 = +(Math.random() * 5).toFixed(9)
+  const r1 = (Math.random() * 5).toFixed(9)
+  const r2 = (Math.random() * 5).toFixed(9)
 
   storiesOf(`TokenItem/${name}`, module)
     .addDecorator(CenterDecor)
