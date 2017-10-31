2becdf41a5f5b653ddd5fd4057507acca62b5ba4
Velenir <Velenir@users.noreply.github.com>
2017-10-31 20:03:49 +0300

    added mock reducer for ratioPairs

---
+++ src/reducers/ratioPairs.ts
@@ -0,0 +1,8 @@
+import { handleActions } from 'redux-actions'
+import { RatioPairs } from 'types'
+
+// TODO: fill in when we have actions
+export default handleActions<RatioPairs>(
+  {},
+  {},
+)
