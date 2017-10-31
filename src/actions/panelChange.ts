0ae3e46b3285b275682b13be3c9b8043836ab498
Velenir <Velenir@users.noreply.github.com>
2017-10-31 20:22:46 +0300

    added workaround for createAction braking Redux DevTools action stringification

--- src/actions/panelChange.ts
+++ src/actions/panelChange.ts
@@ -1,4 +1,8 @@
-import { createAction } from 'redux-actions'
+// import { createAction } from 'redux-actions'
 
 // TODO: fill payload
-export const continueToOrder = createAction<any>('CONTINUE_TO_ORDER')
+// export const continueToOrder = createAction<any>('CONTINUE_TO_ORDER')
+// TODO: breaks Redux devtools
+export const continueToOrder = () => ({
+  type: 'CONTINUE_TO_ORDER',
+})
--- src/actions/tokenOverlay.ts
+++ src/actions/tokenOverlay.ts
@@ -1,6 +1,12 @@
 import { createAction } from 'redux-actions'
-import { TokenMod } from 'types'
+import { TokenMod, TokenCode } from 'types'
 
 export const openOverlay = createAction<{ mod: TokenMod }>('OPEN_OVERLAY')
-export const closeOverlay = createAction('CLOSE_OVERLAY')
-export const selectTokenAndCloseOverlay = createAction<{ mod: TokenMod }>('SELECT_TOKEN_AND_CLOSE_OVERLAY')
+// export const closeOverlay = createAction<{ mod: TokenMod }>('CLOSE_OVERLAY')
+// TODO: breaks Redux devtools
+export const closeOverlay = () => ({
+  type: 'CLOSE_OVERLAY',
+})
+closeOverlay.toString = () => 'CLOSE_OVERLAY'
+export const selectTokenAndCloseOverlay = createAction<{ mod: TokenMod, code: TokenCode }>(
+  'SELECT_TOKEN_AND_CLOSE_OVERLAY')
