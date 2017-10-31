36d180242f91b1549f319d45fc842d3ac35e46cb
Velenir <Velenir@users.noreply.github.com>
2017-10-31 20:11:46 +0300

    changed global typings to better reflect mock store

---
+++ stories/TokenPicker.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react'
+
+import { storiesOf, StoryDecorator } from '@storybook/react'
+// import { array, object, boolean } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
+
+import TokenPicker from 'components/TokenPicker'
+import { TokenBalances, State } from 'types'
+
+import { codeList } from 'globals'
+import { storeInit } from './helpers/mockStore'
+import { Provider } from 'react-redux'
+
+const tokenBalances = codeList.reduce(
+  (acc, code) => (acc[code] = (Math.random() * 5).toFixed(9), acc), {},
+) as TokenBalances
+
+const initialState: Partial<State> = {
+  tokenBalances,
+  tokenPair: {
+    sell: 'ETH',
+    buy: 'GNO',
+  },
+}
+
+const store = storeInit(initialState)
+console.log(store.getState())
+
+const ProviderDecor: StoryDecorator = story => (
+  <Provider store={store}>
+    {story()}
+  </Provider>
+)
+
+
+const CenterDecor: StoryDecorator = story => (
+  <div
+    style={{
+      display: 'flex',
+      height: '100vh',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <div style={{
+      padding: 20,
+      backgroundColor: 'white',
+      width: 550,
+      height: 500,
+      position: 'relative',
+    }}>
+      {story()}
+    </div>
+  </div>
+)
+
+storiesOf('TokenPicker', module)
+  .addDecorator(ProviderDecor)
+  .addDecorator(CenterDecor)
+  .addWithJSX('main', () => <TokenPicker
+    continueToOrder={action('Continue to order details')}
+  />)
