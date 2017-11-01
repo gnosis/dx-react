5ce48858cd9ecb597aa55dc6e36ce5cd20c3ea37
Velenir <Velenir@users.noreply.github.com>
2017-11-01 14:31:21 +0300

    changed stories to work with the new RatioPairs type

--- stories/TopAuctions.tsx
+++ stories/TopAuctions.tsx
@@ -5,30 +5,34 @@ import { object } from '@storybook/addon-knobs'
 
 import TopAuctions from 'components/TopAuctions'
 
-import { code2tokenMap } from 'globals'
+import { codeList } from 'globals'
+import { RatioPairs } from 'types'
 
 /*
 * ratioPairs = {
-*  'GNO/ETH': 0.31214312,
-   'OMG/ETH': 0.01976562,
+*  { buy: 'GNO' sell: 'ETH', price: 0.31214312 },
+   { buy: 'OMG', sell: 'ETH', price: 0.01976562 },
 *  ...
 * }
 */
-const ratioPairs = Object.keys(code2tokenMap).reduce((acc, code) => {
-  if (code === 'ETH') return acc
+const ratioPairs = codeList.reduce((acc, code) => {
+  if (code !== 'ETH') acc.push({
+    sell: 'ETH',
+    buy: code,
+    price: Math.random().toFixed(8),
+  })
 
-  acc[`${code}/ETH`] = +Math.random().toFixed(8)
   return acc
-}, {}) as { [pair: string]: number }
-
+}, []) as RatioPairs
 
 /**
  * sorts pairs by ASC and takes top 5
  * @param {typeof ratioPairs} pairs 
  * @returns {typeof ratioPairs}
  */
-const getTop5Pairs = (pairs: typeof ratioPairs) => Object.keys(pairs).sort((a, b) => pairs[b] - pairs[a])
-  .slice(0, 5).reduce((acc, pair) => (acc[pair] = pairs[pair], acc), {})
+const getTop5Pairs = (pairs: RatioPairs) => pairs.slice()
+  .sort((a, b) => +b.price - +a.price)
+  .slice(0, 5)
 
 const CenterDecor: StoryDecorator = story => (
   <div
@@ -54,6 +58,6 @@ const CenterDecor: StoryDecorator = story => (
 storiesOf(`TopAuctions`, module)
   .addDecorator(CenterDecor)
   .addWithJSX('5 random pairs', () => {
-    const top5Pairs = getTop5Pairs(object('ratioPairs', ratioPairs))
+    const top5Pairs = getTop5Pairs(ratioPairs.map((pair, i) => object(`Pair ${i}`, pair)))
     return <TopAuctions pairs={top5Pairs} />
   })
