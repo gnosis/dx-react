5e365fda1afc8ac75fbc9d11ab546afb0b71575d
Velenir <Velenir@users.noreply.github.com>
2017-11-01 10:02:55 +0300

    proper fix for createAction breaking action stringification

--- src/actions/tokenOverlay.ts
+++ src/actions/tokenOverlay.ts
@@ -2,11 +2,6 @@ import { createAction } from 'redux-actions'
 import { TokenMod, TokenCode } from 'types'
 
 export const openOverlay = createAction<{ mod: TokenMod }>('OPEN_OVERLAY')
-// export const closeOverlay = createAction<{ mod: TokenMod }>('CLOSE_OVERLAY')
-// TODO: breaks Redux devtools
-export const closeOverlay = () => ({
-  type: 'CLOSE_OVERLAY',
-})
-closeOverlay.toString = () => 'CLOSE_OVERLAY'
+export const closeOverlay = createAction<void>('CLOSE_OVERLAY', () => { })
 export const selectTokenAndCloseOverlay = createAction<{ mod: TokenMod, code: TokenCode }>(
   'SELECT_TOKEN_AND_CLOSE_OVERLAY')
