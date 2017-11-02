4a35ca1e11655f361e0212311b848dbff7a73eb2
David Sato <david.sato64@gmail.com>
2017-11-02 13:00:28 +0100

    removed fake data 'tokens' from containers/MenuWallet and added tokens selector to mapStateToProps

--- src/containers/MenuWallet/index.ts
+++ src/containers/MenuWallet/index.ts
@@ -1,22 +1,13 @@
 import { connect } from 'react-redux'
-import { getCurrentBalance, getAccount } from 'selectors/blockchain'
-import { State, Tokens } from 'types'
+import { getCurrentBalance, getAccount, getTokens } from 'selectors/blockchain'
+import { State } from 'types'
 
 import { MenuWallet, WalletProps } from 'components/MenuWallet'
-
-// TODO: move into redux or some global config
-// Will need to grab tokens[name].balance from contracts
-const tokens: Tokens = {
-  GNO: {
-    name: 'GNO',
-    balance: 12,
-  },
-}
 
 const mapStateToProps = (state: State) => ({
   account: getAccount(state),
   balance: getCurrentBalance(state),
-  tokens,
+  tokens: getTokens(state),
 })
 
 export default connect<WalletProps>(mapStateToProps)(MenuWallet)
