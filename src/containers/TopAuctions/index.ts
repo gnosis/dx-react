6a16ec872f1a131b7e89d6d3bb3b45c98dcdd03a
Velenir <Velenir@users.noreply.github.com>
2017-11-02 16:53:08 +0300

    moved selectTop5Pairs to selectors

--- src/containers/TopAuctions/index.ts
+++ src/containers/TopAuctions/index.ts
@@ -1,26 +1,8 @@
 import { connect } from 'react-redux'
 import TopAuctions from 'components/TopAuctions'
 import { selectTokenPair } from 'actions'
-import { State, RatioPairs } from 'types'
-
-import { createSelector } from 'reselect'
-
-
-// TODO: move to selectors
-// TODO: do comparison (+pairs[b] - +pairs[a]) with BigNumber.lt/gt/eq()
-/**
- * sorts pairs by ASC and takes top 5
- * @param {typeof ratioPairs} pairs 
- * @returns {typeof ratioPairs}
- */
-const getTop5Pairs = (pairs: RatioPairs) => pairs.slice()
-  .sort((a, b) => +b.price - +a.price)
-  .slice(0, 5)
-
-const selectTop5Pairs = createSelector(
-  ({ ratioPairs }) => ratioPairs,
-  getTop5Pairs,
-)
+import { State } from 'types'
+import { selectTop5Pairs } from 'selectors/ratioPairs'
 
 const mapStateToProps = (state: State) => ({
   pairs: selectTop5Pairs(state),
