056e015b0a8e43b404a8cfde9276e16931214f98
Velenir <Velenir@users.noreply.github.com>
2017-11-03 17:58:55 +0300

    added typings to be able to import .svg files

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,3 +2,10 @@ interface Window {
   __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any,
   web3: any,
 }
+
+declare module 'storybook-router'
+
+declare module '*.svg' {
+  const content: any
+  export default content
+}
