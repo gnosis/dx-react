ded13c103ddf972c1c3e6eb6051051bb0c3905e0
Velenir <Velenir@users.noreply.github.com>
2017-11-06 11:43:19 +0300

    changed ternary to a helper function

--- src/components/AuctionStatus/index.tsx
+++ src/components/AuctionStatus/index.tsx
@@ -22,32 +22,46 @@ export interface AuctionStatusProps {
 
 const capitalize = (str: string) => str && (str[0].toUpperCase() + str.slice(1))
 
-const AuctionStatus: React.SFC<AuctionStatusProps> = ({ sellToken, buyToken, buyAmount, timeLeft, status }) => (
-  <div className="auctionStatus">
-    <span>
-      <small>AUCTION</small>
-      <big>{sellToken}/{buyToken}</big>
-    </span>
+const showStatus = ({ timeLeft, buyAmount, buyToken, status }: AuctionStatusProps) => {
+  switch (status) {
+    case Status.ACTIVE:
+      return [
+        <h5 key="0">ESTIMATED COMPLETION TIME</h5>,
+        <i key="1">{timeLeft}</i>,
+      ]
+    case Status.ENDED:
+      return (
+        <button id="claimToken">
+          <i>CLAIM</i>
+          <strong>{buyAmount} {buyToken}</strong>
+          <span><img src={claim} /></span>
+        </button>
+      )
+    default:
+      return null
+  }
+}
 
-    <span>
-      <small>STATUS</small>
-      <big data-status={status}>{capitalize(status)}</big>
-    </span>
+const AuctionStatus: React.SFC<AuctionStatusProps> = (props) => {
+  const { sellToken, buyToken, status } = props
 
-    <span>
-      {
-        status === Status.ACTIVE ? [
-          <h5 key="1">ESTIMATED COMPLETION TIME</h5>,
-          <i key="2">{timeLeft}</i>,
-        ] : status === Status.ENDED ?
-            <button id="claimToken">
-              <i>CLAIM</i>
-              <strong>{buyAmount} {buyToken}</strong>
-              <span><img src={claim} /></span>
-            </button> : null
-      }
-    </span>
-  </div>
-)
+  return (
+    <div className="auctionStatus">
+      <span>
+        <small>AUCTION</small>
+        <big>{sellToken}/{buyToken}</big>
+      </span>
+
+      <span>
+        <small>STATUS</small>
+        <big data-status={status}>{capitalize(status)}</big>
+      </span>
+
+      <span>
+        {showStatus(props)}
+      </span>
+    </div>
+  )
+}
 
 export default AuctionStatus
