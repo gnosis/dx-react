70583186063659232b834bb642b4c42dc567b6b0
Velenir <Velenir@users.noreply.github.com>
2017-11-09 14:11:08 +0300

    added typings where there were none

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -4,8 +4,15 @@ interface Window {
 }
 
 declare module 'storybook-router'
+declare module 'web3'
+declare module 'truffle-contract'
 
 declare module '*.svg' {
+  const content: any
+  export default content
+}
+
+declare module '*.json' {
   const content: any
   export default content
 }
