d03157599dd3bb544ebccdea585cba4edab38a45
David Sato <david.sato64@gmail.com>
2017-11-15 17:38:03 +0100

    edited tokenBalances reducer to remove zeroBalance reduce object and added initialState + imported action/handleaction

--- src/reducers/tokenBalances.ts
+++ src/reducers/tokenBalances.ts
@@ -1,11 +1,10 @@
-import { createAction, handleActions } from 'redux-actions'
+import { handleActions } from 'redux-actions'
 import { TokenBalances } from 'types'
-// import { setTokenBalance } from 'actions/tokenBalance'
+
+import { setTokenBalance } from 'actions/tokenBalance'
 // import { codeList } from 'globals'
 
 // const zeroBalance = codeList.reduce((acc, code) => (acc[code] = 0, acc), {}) as TokenBalances
-
-export const setTokenBalance = createAction('SET_TOKEN_BALANCE')
 
 // TODO: fill in when we have actions + fill in rest of Tokens + proper typing
 export default handleActions<TokenBalances | any>(
