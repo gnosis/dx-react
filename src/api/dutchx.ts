57ee95b6c05741b2b5ca40131419f8fcc81be513
David Sato <david.sato64@gmail.com>
2017-11-16 14:44:57 +0100

    Revert "removed useless console.log(s) and unused tokenPair function"
    This reverts commit 97c794e5f7727f520280ea2ca4ff38ddbdd7cc6e.

--- src/api/dutchx.ts
+++ src/api/dutchx.ts
@@ -7,6 +7,7 @@ let promisedDX: Promise<any>
  * @param {*dictionary} DUTCHX_OPTIONS
  */
 export const initDutchXConnection = async (DUTCHX_OPTIONS: any) => {
+  console.log(' ===> FIRING initDutchX ACTION')
   try {
     return promisedDX = dutchX.init(DUTCHX_OPTIONS)
   } catch (e) {
@@ -65,4 +66,59 @@ export const getTokenBalances = async (account: Account) => {
   }
 
   return [ETH, GNO]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const tokenPairSelect = async (
+  contract: string, token1: string, token2: string, amount: number, proposedVal: number,
+) => {
+
+  console.log(contract, token1, token2, amount, proposedVal)
+
+  const dx = await getDutchXConnection()
+
+  // Accts to test with HttpProvider - if using Metamask you must check the testrpc accounts and add manually
+  const accts = [...dx.web3.eth.accounts]
+  const defaults = { from: accts[0], gas: 4712388, gasPrice: 100000000000 }
+  console.log(accts)
+
+  const Contracts = dx.contracts
+  const initialiser = accts[0]
+
+  const seller = accts[1]
+  const sellToken = await Contracts.Token.new(defaults)
+  console.log(sellToken)
+  await sellToken.approve(seller, 100, defaults)
+  await sellToken.transferFrom(initialiser, seller, 100, defaults)
+
+  const buyer = accts[2]
+  const buyToken = await Contracts.Token.new(defaults)
+  await buyToken.approve(buyer, 100, defaults)
+  await buyToken.transferFrom(initialiser, buyer, 1000, defaults)
+
+  const DUTCHX = await Contracts.Token.new(defaults)
+
+  // create dx
+  const newDX = await Contracts.DutchExchange.new(2, 1, sellToken.address, buyToken.address, DUTCHX.address, defaults)
+  const dxa = newDX.address
+
+  console.log(dxa)
 }
