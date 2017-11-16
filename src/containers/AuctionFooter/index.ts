a0b448e71eac4867351709bed590965abb1ff104
Velenir <Velenir@users.noreply.github.com>
2017-11-16 16:42:31 +0300

    connected AuctionFooter to store

---
+++ src/containers/AuctionFooter/index.ts
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux'
+import AuctionFooter from 'components/AuctionFooter'
+import { State } from 'types'
+
+const mapStateToProps = ({ tokenPair: { sell, buy } }: State) => ({
+  sellToken: sell,
+  buyToken: buy,
+  // TODO: get from redux store
+  sellAmount: '1',
+  buyAmount: '2.5520300',
+  // TODO: compare with Status.ENDED
+  auctionEnded: false,
+})
+
+
+export default connect(mapStateToProps)(AuctionFooter)
