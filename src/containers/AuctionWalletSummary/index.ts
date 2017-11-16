3bdaca3873cfb65e3b5c0e42b7a0c275cd74abdf
Velenir <Velenir@users.noreply.github.com>
2017-11-16 21:08:31 +0300

    fixed account address not being passed to component

--- src/containers/AuctionWalletSummary/index.ts
+++ src/containers/AuctionWalletSummary/index.ts
@@ -7,7 +7,7 @@ const mapState = ({ blockchain }: State) => {
   const { activeProvider, providers } = blockchain
   const currentProvider = providers[activeProvider]
 
-  const { name: provider = '', address = '', network = '', loaded, available } = currentProvider
+  const { name: provider = '', account: address = '', network = '', loaded, available } = currentProvider
 
   return ({
     address,
