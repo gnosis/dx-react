134d5e0fe97ffe364b80cf30fdd8a23e87fd0d8f
David Sato <david.sato64@gmail.com>
2017-11-16 14:45:50 +0100

    Revert "dxInst --> promisedDX to be called via await getDutchXConnection() for current instance || getCurrentBalance now returns TokenETH balance as opposed to web3.eth.currentBalance"
    This reverts commit f5bc1676242dfa586a7eb2bc922d336ccd3a77e5.

--- src/api/dutchx.ts
+++ src/api/dutchx.ts
@@ -1,6 +1,7 @@
 import dutchX from './initialization'
+import { weiToEth } from 'utils/helpers'
 
-let promisedDX: Promise<any>
+let dxInst: any
 
 /**
  * Initializes connection to DutchX
@@ -9,7 +10,9 @@ let promisedDX: Promise<any>
 export const initDutchXConnection = async (DUTCHX_OPTIONS: any) => {
   console.log(' ===> FIRING initDutchX ACTION')
   try {
-    return promisedDX = dutchX.init(DUTCHX_OPTIONS)
+    dxInst = await dutchX.init(DUTCHX_OPTIONS)
+    return dxInst
+    // console.log('SUCCESS CONNECTING TO DUTCHX INSTANCE', dxInst)
   } catch (e) {
     console.log('ERROR CONNECTING TO DUTCHX INSTANCE', e.message)
     throw (e)
@@ -19,43 +22,48 @@ export const initDutchXConnection = async (DUTCHX_OPTIONS: any) => {
 /**
  * Returns an instance of the connection to DutchX
  */
-export const getDutchXConnection = () => promisedDX
+export const getDutchXConnection: Function = async (): Promise<Object> => dxInst
 
 /**
  * Returns the default node account
  */
 export const getCurrentAccount = async () => {
-  const dx = await getDutchXConnection()
+  const dutchX = await getDutchXConnection()
   
-  return await new Promise((resolve, reject) => dx.web3.eth.getAccounts(
+  return await new Promise((resolve, reject) => dutchX.web3.eth.getAccounts(
     (e: Object, accounts: Object) => (e ? reject(e) : resolve(accounts[0]))),
   )
 }
 
 export const getAllAccounts = async () => {
-  const dx = await getDutchXConnection()
+  const dutchX = await getDutchXConnection()
 
-  const accounts = await new Promise((resolve, reject) => 
-    dx.web3.eth.getAccounts((err: any, accts: any) => {
-      err ? reject(err) : resolve(accts)
-    }),
-  )
+  const accounts = await new Promise((resolve, reject) => dutchX.web3.eth.getAccounts((err: any, accts: any) => {
+    err ? reject(err) : resolve(accts)
+  }))
   console.log(accounts)
 }
 
 /**
- * Returns the account balance in ETHER from TokenETH
+ * Returns the account balance
  */
-export const getCurrentBalance = async (account: Account) => {
-  const dx = await getDutchXConnection()
-
-  return await (await dx.TokenETH.balanceOf(account)).toNumber()
+export const getCurrentBalance = async (account: Object) => {
+  const dutchX = await getDutchXConnection()
+  console.log('GET CONNECTION DUTCHX INSTANCE = ', dutchX)
+  return await new Promise((accept, reject) => dutchX.web3.eth.getBalance(
+    account,
+    (e: Object, balance: Object) => (
+      e ? reject(e) : accept(weiToEth(balance.toString()))
+    ),
+  ))
 }
 
 // TODO: probably extrapolate some parameterizable parts - ContractName in string form for example
 export const getTokenBalances = async (account: Account) => {
-  const dx = await getDutchXConnection()
+  // grab the TokenContracts from dx
+  // forEach contract call the Contract.balanceOf() passing in the account address
 
+  const dx = await getDutchXConnection()
   const ETH = {
     name: 'ETH', 
     balance: (await dx.TokenETH.balanceOf(account)).toNumber(),
@@ -68,39 +76,20 @@ export const getTokenBalances = async (account: Account) => {
   return [ETH, GNO]
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 export const tokenPairSelect = async (
   contract: string, token1: string, token2: string, amount: number, proposedVal: number,
 ) => {
 
   console.log(contract, token1, token2, amount, proposedVal)
 
-  const dx = await getDutchXConnection()
+  const dutchX = await getDutchXConnection()
 
   // Accts to test with HttpProvider - if using Metamask you must check the testrpc accounts and add manually
-  const accts = [...dx.web3.eth.accounts]
+  const accts = [...dutchX.web3.eth.accounts]
   const defaults = { from: accts[0], gas: 4712388, gasPrice: 100000000000 }
   console.log(accts)
 
-  const Contracts = dx.contracts
+  const Contracts = dutchX.contracts
   const initialiser = accts[0]
 
   const seller = accts[1]
@@ -117,8 +106,8 @@ export const tokenPairSelect = async (
   const DUTCHX = await Contracts.Token.new(defaults)
 
   // create dx
-  const newDX = await Contracts.DutchExchange.new(2, 1, sellToken.address, buyToken.address, DUTCHX.address, defaults)
-  const dxa = newDX.address
+  const dx = await Contracts.DutchExchange.new(2, 1, sellToken.address, buyToken.address, DUTCHX.address, defaults)
+  const dxa = dx.address
 
   console.log(dxa)
 }
