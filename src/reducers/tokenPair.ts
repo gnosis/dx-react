8e5b0b866d5590af8db87d2b5a00fe2b3867b853
Velenir <Velenir@users.noreply.github.com>
2017-11-17 17:19:22 +0300

    added reducer for setSellTokenAmount action

--- src/reducers/tokenPair.ts
+++ src/reducers/tokenPair.ts
@@ -1,6 +1,6 @@
 import { handleActions } from 'redux-actions'
 
-import { selectTokenAndCloseOverlay, selectTokenPair } from 'actions'
+import { selectTokenAndCloseOverlay, selectTokenPair, setSellTokenAmount } from 'actions'
 import { TokenPair } from 'types'
 import { TokenItemProps } from 'components/TokenItem'
 
@@ -11,12 +11,21 @@ export default handleActions<TokenPair, TokenItemProps & TokenPair>(
       return {
         ...state,
         [mod]: code,
+        sellAmount: '0',
       }
     },
-    [selectTokenPair.toString()]: (_, action) => action.payload,
+    [selectTokenPair.toString()]: (_, action) => ({
+      ...action.payload,
+      sellAmount: '0',
+    }),
+    [setSellTokenAmount.toString()]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
   },
   {
     sell: 'ETH',
     buy: 'GNO',
+    sellAmount: '0',
   },
 )
