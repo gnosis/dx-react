6d78f7fbb571fc69ab05f27527da06ce392db8ee
Velenir <Velenir@users.noreply.github.com>
2017-11-17 18:45:41 +0300

    added Link component in place of anchor to allow navigation without page reload

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,11 +3,12 @@ import * as React from 'react'
 import MenuWallet from 'containers/MenuWallet'
 import MenuAuctions from 'containers/MenuAuctions'
 import Hamburger from 'components/Hamburger'
+import { Link } from 'react-router-dom'
 
 export const Header: React.SFC = () => (
   <header>
     <div>
-      <a href="#" title="DutchX - Dutch Auction Exchange" className="logo"></a>
+      <Link to="/" title="DutchX - Dutch Auction Exchange" className="logo"></Link>
       <MenuWallet />
       <MenuAuctions />
       <Hamburger />
