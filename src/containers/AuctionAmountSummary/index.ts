d26cdb3b614a48a2a11a03dfd08b928315922c92
Velenir <Velenir@users.noreply.github.com>
2017-11-17 13:39:40 +0300

    connected AuctionAmountSummary to store

---
+++ src/containers/AuctionAmountSummary/index.ts
@@ -0,0 +1,29 @@
+import { connect } from 'react-redux'
+import { createSelector } from 'reselect'
+
+import { State, RatioPairs, TokenCode } from 'types'
+import AuctionAmountSummary from 'components/AuctionAmountSummary'
+
+// TODO: move to selectors
+const findRatioPair = createSelector(
+  ({ tokenPair }) => tokenPair.sell,
+  ({ tokenPair }) => tokenPair.buy,
+  ({ ratioPairs }) => ratioPairs,
+  (sell: TokenCode, buy: TokenCode, ratioPairs: RatioPairs) => ratioPairs.find(
+    pair => pair.sell === sell && pair.buy === buy),
+)
+
+const mapState = (state: State) => {
+  // TODO: always have some price for every pair in RatioPairs
+  const { sell, buy, price, sellAmount } = findRatioPair(state) || Object.assign({ price: 2 }, state.tokenPair)
+  return ({
+    // TODO: change prop to sellTokenBalance
+    sellToken: sell,
+    buyToken: buy,
+    sellTokenAmount: sellAmount,
+    // TODO: use BN.mult() inside component
+    buyTokenAmount: (+price * +sellAmount).toString(),
+  })
+}
+
+export default connect(mapState)(AuctionAmountSummary)
