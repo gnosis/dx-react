8624b939779858818cd8f6f02e5d92df7e20e790
Velenir <Velenir@users.noreply.github.com>
2017-11-20 18:16:05 +0300

    connected WalletPanel to submitSellOrder action

--- src/containers/WalletPanel/index.ts
+++ src/containers/WalletPanel/index.ts
@@ -1,11 +1,20 @@
-import { connect } from 'react-redux'
+import { connect, Dispatch } from 'react-redux'
 import WalletPanel from 'components/WalletPanel'
 // import { State } from 'types'
+import { submitSellOrder } from 'actions'
 
 const mapStateToProps = () => ({
   // TODO: get address from store, populated by contract addresses
   auctionAddress: '0x03494929349594',
 })
 
+const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
+  submitOrder() {
+    console.log('Submitting order')
 
-export default connect(mapStateToProps)(WalletPanel)
+    dispatch(submitSellOrder())
+  }
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(WalletPanel)
