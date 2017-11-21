25fc07e7a6e4e5e8e93631e5a0040e9a38a2d735
David Sato <david.sato64@gmail.com>
2017-11-21 17:43:43 +0100

    changed to only fund Seller account and not Buyer so as to make it easier

--- migrations/3_fund_accounts.js
+++ migrations/3_fund_accounts.js
@@ -5,25 +5,25 @@ module.exports = (deployer, network, accounts) => {
   let ETH
   let GNO
 
-  const [master, seller, buyer] = accounts
+  const [master, seller] = accounts
   deployer.then(() => {
     TokenETH.deployed().then((inst) => {
       ETH = inst
-      ETH.approve(seller, 100, { from: master })
+      ETH.approve(seller, 1000, { from: master })
     })
   })
   deployer.then(() => {
     TokenGNO.deployed().then((inst) => {
       GNO = inst
-      GNO.approve(buyer, 10000, { from: master })
+      GNO.approve(seller, 10000, { from: master })
     })
   })
 
-  deployer.then(() => ETH.transferFrom(master, seller, 100, { from: seller }))
-  deployer.then(() => GNO.transferFrom(master, buyer, 1000, { from: buyer }))
+  deployer.then(() => ETH.transferFrom(master, seller, 1000, { from: seller }))
+  deployer.then(() => GNO.transferFrom(master, seller, 1000, { from: seller }))
 
   deployer.then(() => ETH.balanceOf(seller))
     .then(bal => console.log('Seller ETH balance', bal.toNumber()))
-  deployer.then(() => GNO.balanceOf(buyer))
-    .then(bal => console.log('Buyer GNO balance', bal.toNumber()))
+  deployer.then(() => GNO.balanceOf(seller))
+    .then(bal => console.log('Seller GNO balance', bal.toNumber()))
 }
