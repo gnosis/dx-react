9498134980f4a2b7c784afdc686b30d658abc097
David Sato <david.sato64@gmail.com>
2017-11-23 20:26:55 +0100

    on rethink - not running with ratioPairs reducer for now until further testing - commented out

--- src/reducers/ratioPairs.ts
+++ src/reducers/ratioPairs.ts
@@ -1,12 +1,12 @@
 import { handleActions } from 'redux-actions'
 import { RatioPairs } from 'types'
 
-import { setClosingPrice } from 'actions/ratioPairs'
+// import { setClosingPrice } from 'actions/ratioPairs'
 
 // TODO: fill in when we have actions
 export default handleActions<RatioPairs>(
   {
-    [setClosingPrice.toString()]: (state, action) => [...state, action.payload] as any,
+    // [setClosingPrice.toString()]: (state, action) => [...state, action.payload] as any,
   },
   [],
 )
