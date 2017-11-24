81a4be4f743fb589edc9f5c78ebeb03c735d79a6
Velenir <Velenir@users.noreply.github.com>
2017-11-24 17:27:17 +0300

    added flags to post sell order from buyer and custom account

--- trufflescripts/sell_order.js
+++ trufflescripts/sell_order.js
@@ -1,13 +1,15 @@
 const DutchExchangeETHGNO = artifacts.require('./DutchExchangeETHGNO.sol')
 const TokenETH = artifacts.require('./TokenETH.sol')
 
-const argv = require('minimist')(process.argv.slice(2))
+const argv = require('minimist')(process.argv.slice(2), { string: 'a' })
 
 /**
  * truffle exec trufflescripts/buy_order.js
- * to post a buy order to the current auction
+ * to post a buy order to the current auction as the seller
  * @flags:
  * -n <number>   for a specific amount
+ * --buyer      as the buyer
+ * -a <address>  as the given account
  */
 
 
@@ -17,7 +19,12 @@ module.exports = async () => {
 
   const auctionIndex = (await dx.auctionIndex()).toNumber()
 
-  const [, seller] = web3.eth.accounts
+  let seller
+  if (argv.a) seller = argv.a
+  else if (argv.buyer)[, , seller] = web3.eth.accounts
+  else {
+    [, seller] = web3.eth.accounts
+  }
 
   const sellerStats = () => Promise.all([
     dx.sellVolumeCurrent(),
