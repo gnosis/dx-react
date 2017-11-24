f5a7b4ebac0d4dc8cdd1c021e7600796c92a518e
David Sato <david.sato64@gmail.com>
2017-11-24 14:38:23 +0100

    replaced selectTokenPair w/selectTokenPairAndSetClosingPrice thunk --> component/TopAuctions --> containers/TopAuctions

--- src/components/TopAuctions/index.tsx
+++ src/components/TopAuctions/index.tsx
@@ -3,17 +3,17 @@ import { RatioPairs, TokenPair } from 'types'
 
 export interface TopAuctionsProps {
   pairs: RatioPairs,
-  selectTokenPair(pair: Pick<TokenPair, 'sell' | 'buy'>): any
+  selectTokenPairAndSetClosingPrice(pair: Pick<TokenPair, 'sell' | 'buy'>): any
 }
 
-const TopAuctions: React.SFC<TopAuctionsProps> = ({ pairs, selectTokenPair }) => (
+const TopAuctions: React.SFC<TopAuctionsProps> = ({ pairs, selectTokenPairAndSetClosingPrice }) => (
   Object.keys(pairs).length > 0 &&
   <div className="topAuctions">
     <h3>HIGH VOLUME TOKEN PAIR AUCTIONS</h3>
     <ul>
       {pairs.map(({ sell, buy, price }) => {
         const pair = `${buy}/${sell}`
-        return <li key={pair} onClick={() => selectTokenPair({ sell, buy })}><strong>{pair}</strong> {price}</li>
+        return <li key={pair} onClick={() => selectTokenPairAndSetClosingPrice({ sell, buy })}><strong>{pair}</strong> {price}</li>
       })}
     </ul>
   </div>
