edddcf81353732cd133c13f95d5f17620cd7c1a6
David Sato <david.sato64@gmail.com>
2017-11-24 14:35:53 +0100

    created selectTokenPair and calc closingPrice for TopAuction selection

--- src/actions/tokenPair.ts
+++ src/actions/tokenPair.ts
@@ -1,5 +1,19 @@
 import { createAction } from 'redux-actions'
 import { TokenPair } from 'types'
 
+import { closingPrice } from 'api/dutchx'
+import { setClosingPrice } from 'actions/ratioPairs'
+
 export const selectTokenPair = createAction<TokenPair>('SELECT_TOKEN_PAIR')
 export const setSellTokenAmount = createAction<{ sellAmount: number }>('SET_SELL_TOKEN_AMOUNT')
+
+export const selectTokenPairAndSetClosingPrice = (tokenPair: TokenPair) => async (dispatch: Function) => {
+  const { sell, buy } = tokenPair
+  try {
+    const price = (await closingPrice(sell, buy)).toString()
+    await dispatch(setClosingPrice({ sell, buy, price }))
+    await dispatch(selectTokenPair({ buy, sell } as TokenPair))
+  } catch (e) {
+    console.warn(e)
+  }
+}
