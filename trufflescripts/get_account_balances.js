67bdcf38beee945ca77c155a94b1a25c159ce901
Velenir <Velenir@users.noreply.github.com>
2017-11-24 14:39:09 +0300

    added -a account flag to get balances for a sepcific account

--- trufflescripts/get_account_balances.js
+++ trufflescripts/get_account_balances.js
@@ -1,6 +1,15 @@
 const TokenETH = artifacts.require('./TokenETH.sol')
 const TokenGNO = artifacts.require('./TokenGNO.sol')
 
+const argv = require('minimist')(process.argv.slice(2), { string: 'a' })
+
+/**
+ * truffle exec trufflescripts/get_account_balances.js
+ * get ETH and GNO balances for seller and buyer accounts
+ * @flags:
+ * -a <address>       and for the given account
+ */
+
 module.exports = async () => {
   // web3 is available in the global context
   const [, seller, buyer] = web3.eth.accounts
@@ -16,4 +25,12 @@ module.exports = async () => {
 
   console.log(`Seller:\t${sellerETHBalance}\tETH,\t${sellerGNOBalance}\tGNO`)
   console.log(`Buyer:\t${buyerETHBalance}\tETH,\t${buyerGNOBalance}\tGNO`)
+
+  if (argv.a) {
+    const accountETHBalance = (await eth.balanceOf(argv.a)).toNumber()
+    const accountGNOBalance = (await gno.balanceOf(argv.a)).toNumber()
+
+    console.log(`\nAccount at ${argv.a} address`)
+    console.log(`Balance:\t${accountETHBalance}\tETH,\t${accountGNOBalance}\tGNO`)
+  }
 }
