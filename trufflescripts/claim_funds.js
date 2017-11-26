f67dd2289fa34b4a50601cdd7d9321cfb2ae6872
Velenir <Velenir@users.noreply.github.com>
2017-11-26 22:32:43 +0300

    fixed log indentation

--- trufflescripts/claim_funds.js
+++ trufflescripts/claim_funds.js
@@ -67,7 +67,7 @@ module.exports = async () => {
       await dx.claimBuyerFunds(auctionIndex, { from: buyer });
 
       [buyerBalance, buyerClaimed] = await buyerStats()
-      console.log(`    is:  ${buyerBalance} ${buyerClaimed}`)
+      console.log(`    is:\t\t${buyerBalance}\t${buyerClaimed}`)
     } catch (error) {
       console.error(error.message || error)
     }
