988a67e96b19a6e5f8f74d514ae56ea91728e4c4
David <david.sato64@gmail.com>
2017-11-27 18:38:58 +0100

    removed async

--- trufflescripts/revert_auction.js
+++ trufflescripts/revert_auction.js
@@ -3,8 +3,8 @@ const { revertSnapshot } = require('./utils')(web3)
 
 module.exports = async () => {
   try {
-    const reverted = await revertSnapshot()
-    console.log('REVERTING SNAPSHOT - NOW AT BLOCK #', reverted)
+    const reverted = revertSnapshot()
+    console.log('REVERTING SNAPSHOT - NOW AT BLOCK-ID #', reverted)
   } catch(e) {
     console.log(`ERROR REVERTING: ${e}`)
   }
