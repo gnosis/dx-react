5f65b647517a657da645fee55440b56c98ed4209
Velenir <Velenir@users.noreply.github.com>
2017-11-27 15:43:00 +0300

    added time utilities for testrpc

---
+++ trufflescripts/utils/index.js
@@ -0,0 +1,34 @@
+module.exports = (web3) => {
+  const getTime = (blockNumber = web3.eth.blockNumber) => web3.eth.getBlock(blockNumber).timestamp
+
+  const increaseTimeBy = (seconds) => {
+    web3.currentProvider.send({
+      jsonrpc: '2.0',
+      method: 'evm_increaseTime',
+      params: [seconds],
+      id: 0,
+    })
+
+    web3.currentProvider.send({
+      jsonrpc: '2.0',
+      method: 'evm_mine',
+      params: [],
+      id: 0,
+    })
+  }
+
+  const setTime = (seconds) => {
+    const increaseBy = seconds - getTime()
+    if (increaseBy < 0) {
+      throw new Error('Can\'t decrease time in testrpc')
+    }
+
+    increaseTimeBy(increaseBy)
+  }
+
+  return {
+    getTime,
+    increaseTimeBy,
+    setTime,
+  }
+}
