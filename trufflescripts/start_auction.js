349ea9b7ceca62a9d60d480e4f5bee9ed298d7c8
David Sato <david.sato64@gmail.com>
2017-11-29 12:53:26 +0100

    changed file names to make it easier to call || changed utils/index revertSnapshot to async Promise || changed revert.js to accept process params || removed snapshot from start_auction and created snapshot.js to snapshot anyhere

--- trufflescripts/start_auction.js
+++ trufflescripts/start_auction.js
@@ -1,5 +1,5 @@
 const DutchExchangeETHGNO = artifacts.require('./DutchExchangeETHGNO.sol')
-const { getTime, increaseTimeBy, makeSnapshot } = require('./utils')(web3)
+const { getTime, increaseTimeBy } = require('./utils')(web3)
 
 /**
  * truffle exec trufflescripts/start_auction.js
@@ -19,8 +19,6 @@ module.exports = async () => {
 
   // auctionStart is in the future
   if (timeUntilStart > 0) {
-    const snapshot = makeSnapshot()
-    console.log(`SNAPSHOT CREATED WITH BLOCK-ID # ${snapshot}`)
     increaseTimeBy(timeUntilStart + hour)
     console.log(`ETH -> GNO auction ${auctionIndex} started`)
   } else {
