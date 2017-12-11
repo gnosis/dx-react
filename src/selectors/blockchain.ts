622f7fdc9ff99268b2411b4e076f8501d82cfef3
David Sato <david.sato64@gmail.com>
2017-12-11 11:18:11 +0100

    removed Decimal.js implementation and unused files

--- src/selectors/blockchain.ts
+++ src/selectors/blockchain.ts
@@ -1,7 +1,6 @@
 // import { getCurrentAccount } from '../api/dutchx'
 import { State } from 'types'
 import { /* get, */ find, orderBy } from 'lodash'
-const Decimal = require('decimal.js')
 
 export enum ProviderName { METAMASK = 'METAMASK', MIST = 'MIST' }
 
@@ -36,31 +35,3 @@ export const getAccount = (state: State) => selector(state).currentAccount
 
 // Default account balance
 export const getCurrentBalance = (state: State) => selector(state).currentBalance
-
-// Checks GnosisJS initialization
-export const isGnosisInitialized = (state: State) => selector(state).gnosisInitialized
-
-export const getGasCosts = (state: State) => {
-  const gasCosts = selector(state).gasCosts
-  return Object.keys(gasCosts).reduce((acc, item) =>
-    ({ ...acc, [item]: gasCosts[item] ? gasCosts[item] : new Decimal(0) }), {})
-}
-
-export const isGasCostFetched = (state: State, property: string) => selector(state).gasCosts[property] !== undefined
-
-export const getGasPrice = (state: State) => (
-  selector(state).gasPrice ? new Decimal(parseInt(selector(state).gasPrice, 10)) : new Decimal(0)
-)
-
-export const isGasPriceFetched = (state: State) => selector(state).gasPrice !== undefined
-
-export const getEtherTokensAmount = (state: State, account: string) => {
-  if (isGnosisInitialized(state)) {
-    return selector(state).etherTokens !== undefined ? selector(state).etherTokens[account] : new Decimal(0)
-  }
-  return new Decimal(0)
-}
-
-export default {
-  getDefaultAccount,
-}
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,19 +1,18 @@
 import { WALLET_PROVIDER } from 'integrations/constants'
-import Decimal from 'decimal.js'
-const Web3 = require('web3')
+import Web3 from 'web3'
 
 export const getDutchXOptions = (provider: any) => {
   console.log('FIRING getDutchXOptions')
   const opts: any = {}
 
   if (provider && provider.name === WALLET_PROVIDER.METAMASK) {
-      // Inject window.web3
+    // Inject window.web3
     opts.ethereum = window.web3.currentProvider
   } else if (provider && provider === WALLET_PROVIDER.PARITY) {
-      // Inject window.web3
+    // Inject window.web3
     opts.ethereum = window.web3.currentProvider
   } else {
-      // Default remote node
+    // Default remote node
     opts.ethereum = new Web3(
       new Web3.providers.HttpProvider(`${process.env.ETHEREUM_URL}`),
     ).currentProvider
@@ -25,24 +24,5 @@ export const getDutchXOptions = (provider: any) => {
 export const timeoutCondition = (timeout: any, rejectReason: any) => new Promise((_, reject) => {
   setTimeout(() => {
     reject(rejectReason)
-  },         timeout)
+  }, timeout)
 })
-
-/**
- * Converts a value from WEI to ETH
- * @param {String|Number} value
- */
-export const weiToEth = (value: Object | number | any) => {
-  let ethValue = new Decimal(0)
-  if (typeof value === 'string') {
-    ethValue = new Decimal(value)
-    if (ethValue.gt(0)) {
-      return ethValue.div(1e18).toString()
-    }
-    return new Decimal(0).div(1e18).toString()
-  }
-  if (value instanceof Decimal && value.gt(0)) {
-    return value.div(1e18).toString()
-  }
-  return ethValue.toString()
-}
