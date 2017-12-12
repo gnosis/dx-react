20f4f2da669e9b87fd52a146da391edddd60c5e7
David Sato <david.sato64@gmail.com>
2017-12-12 16:55:15 +0100

    fixed get_auction_stats to show correct info

--- trufflescripts/get_auction_stats.js
+++ trufflescripts/get_auction_stats.js
@@ -1,6 +1,6 @@
-const DutchExchangeETHGNO = artifacts.require('./DutchExchangeETHGNO.sol')
-const TokenETH = artifacts.require('./TokenETH.sol')
-const TokenGNO = artifacts.require('./TokenGNO.sol')
+const DutchExchange = artifacts.require('DutchExchange')
+const TokenETH = artifacts.require('EtherToken')
+const TokenGNO = artifacts.require('TokenGNO')
 const { getTime } = require('./utils')(web3)
 
 const getTimeStr = (timestamp) => {
@@ -17,8 +17,9 @@ const getTimeStr = (timestamp) => {
  * prints stats for the current and past auctions
  */
 
+/* eslint no-console: 0 */
 module.exports = async () => {
-  const dx = await DutchExchangeETHGNO.deployed()
+  const dx = await DutchExchange.deployed()
   const eth = await TokenETH.deployed()
   const gno = await TokenGNO.deployed()
 
@@ -27,29 +28,30 @@ module.exports = async () => {
 
   console.log(`Auction holds:\t${dxETHBalance} ETH\t${dxGNOBalance} GNO`)
 
-  const auctionStart = (await dx.auctionStart()).toNumber()
+  const auctionStart = (await dx.auctionStarts.call(eth.address, gno.address)).toNumber()
   const now = getTime()
 
   const timeUntilStart = auctionStart - now
   const timeStr = getTimeStr(timeUntilStart * 1000)
-
-  const auctionIndex = (await dx.auctionIndex()).toNumber()
-
+  // console.log(timeStr)
+  const auctionIndex = (await dx.latestAuctionIndices.call(eth.address, gno.address)).toNumber()
+  // console.log(auctionIndex)
   const [, seller, buyer] = web3.eth.accounts
 
-  const sellVolumeCurrent = (await dx.sellVolumeCurrent()).toNumber()
-  const sellVolumeNext = (await dx.sellVolumeNext()).toNumber()
-  const sellerBalanceNext = sellVolumeNext && (await dx.sellerBalances(auctionIndex + 1, seller)).toNumber()
+  const sellVolumeCurrent = (await dx.sellVolumes.call(eth.address, gno.address, auctionIndex)).toNumber()
+  const sellVolumeNext = (await dx.sellVolumes.call(eth.address, gno.address, auctionIndex + 1)).toNumber()
+  const sellerBalanceNext = sellVolumeNext &&
+    (await dx.sellerBalances(eth.address, gno.address, auctionIndex + 1, seller)).toNumber()
 
   console.log(`
-Current auction index ${auctionIndex}
-  ______________________________________
-  now:\t\t\t${new Date(now * 1000).toTimeString()}
-  auctionStart:\t\t${new Date(auctionStart * 1000).toTimeString()}
-  ${timeUntilStart > 0 ? `starts in\t\t${timeStr}` : timeUntilStart < 0 ? `started\t\t${timeStr}ago` : 'just started'}
-  
-  sellVolumeCurrent:\t${sellVolumeCurrent}
-  sellVolumeNext:\t${sellVolumeNext}${sellerBalanceNext ? `\n  sellerBalance for next auction:\t${sellerBalanceNext}` : ''}  
+    Current auction index ${auctionIndex}
+    ______________________________________
+    now:\t\t\t${new Date(now * 1000).toTimeString()}
+    auctionStart:\t\t${new Date(auctionStart * 1000).toTimeString()}
+    ${timeUntilStart > 0 ? `starts in\t\t${timeStr}` : timeUntilStart < 0 ? `started\t\t${timeStr}ago` : 'just started'}
+    
+    sellVolumeCurrent:\t${sellVolumeCurrent}
+    sellVolumeNext:\t${sellVolumeNext}${sellerBalanceNext ? `\n  sellerBalance for next auction:\t${sellerBalanceNext}` : ''}  
   `)
 
 
@@ -57,11 +59,12 @@ Current auction index ${auctionIndex}
   const indexes = Array.from({ length: auctionIndex }, (v, i) => auctionIndex - i)
 
   const readStats = async (i) => {
-    const buyVolume = (await dx.buyVolumes(i)).toNumber()
+    const buyVolume = (await dx.buyVolumes(eth.address, gno.address, i)).toNumber()
 
     let price, amountToClearAuction, timeUntilAuctionClears
     try {
-      const [num, den] = (await dx.getPrice(i)).map(n => n.toNumber())
+      const [num, den] = (await dx.getPrice(eth.address, gno.address, i)).map(n => n.toNumber())
+      console.log(`DENOMINATOR = ${den}, NUMERATOR = ${num}`)
       price = `1 ETH = ${(num / den).toFixed(8)} GNO`
 
       // if current running auction
@@ -79,10 +82,10 @@ Current auction index ${auctionIndex}
       price += `\n  last closingPrice:\t1 ETH = ${(num / den).toFixed(8)} GNO`
     }
 
-    const sellerBalance = (await dx.sellerBalances(i, seller)).toNumber()
-    const buyerBalance = (await dx.buyerBalances(i, buyer)).toNumber()
-    const sellerClaimed = (await dx.claimedAmounts(i, seller)).toNumber()
-    const buyerClaimed = (await dx.claimedAmounts(i, buyer)).toNumber()
+    const sellerBalance = (await dx.sellerBalances(eth.address, gno.address, i, seller)).toNumber()
+    const buyerBalance = (await dx.buyerBalances(eth.address, gno.address, i, buyer)).toNumber()
+    // const sellerClaimed = (await dx.claimedAmounts(i, seller)).toNumber()
+    // const buyerClaimed = (await dx.claimedAmounts(i, buyer)).toNumber()
 
     if (i !== auctionIndex) {
       console.log('=============================')
@@ -90,12 +93,12 @@ Current auction index ${auctionIndex}
     }
 
     console.log(`
-  buyVolume:\t\t${buyVolume}
-  price:\t\t${price}${amountToClearAuction ? `\n  to clear auction buy\t${amountToClearAuction} GNO` : ''}
-  ${timeUntilAuctionClears ? `will clear with time in ${timeUntilAuctionClears}` : ''}
+      buyVolume:\t\t${buyVolume}
+      price:\t\t${price}${amountToClearAuction ? `\n  to clear auction buy\t${amountToClearAuction} GNO` : ''}
+      ${timeUntilAuctionClears ? `will clear with time in ${timeUntilAuctionClears}` : ''}
 
-  sellerBalance:  ${sellerBalance}\tclaimed:  ${sellerClaimed} ETH
-  buyerBalance:   ${buyerBalance}\tclaimed:  ${buyerClaimed} GNO
+      sellerBalance:  ${sellerBalance}\tclaimed:  ${'SellerClaimed'} ETH
+      buyerBalance:   ${buyerBalance}\tclaimed:  ${'buyerClaimed'} GNO
     `)
   }
 
