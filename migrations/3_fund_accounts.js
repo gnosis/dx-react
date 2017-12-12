3073bd267566284a9cec23f7a6b74bf5a99dcb1c
David Sato <david.sato64@gmail.com>
2017-12-12 11:18:39 +0100

    added funding of MASTER account w/50000 ether, requires funded ganache start

--- migrations/3_fund_accounts.js
+++ migrations/3_fund_accounts.js
@@ -1,12 +1,15 @@
-const EtherToken = artifacts.require('./Tokens/EtherToken.sol')
+/* eslint no-console:0 */
+const EtherToken = artifacts.require('EtherToken')
 // use old TokenGNO contract for testing
-const TokenGNO = artifacts.require('./TokenGNO.sol')
+const TokenGNO = artifacts.require('TokenGNO')
 
 module.exports = (deployer, network, accounts) => {
+  // let DX
   let ETH
   let GNO
 
   const [master, seller] = accounts
+
   deployer.then(() => {
     EtherToken.deployed().then((inst) => {
       ETH = inst
@@ -20,9 +23,12 @@ module.exports = (deployer, network, accounts) => {
     })
   })
 
+  deployer.then(() => ETH.deposit({ value: 50000, from: master }))
   deployer.then(() => ETH.deposit({ value: 1000, from: seller }))
   deployer.then(() => GNO.transferFrom(master, seller, 1000, { from: seller }))
 
+  deployer.then(() => ETH.balanceOf(master))
+    .then(bal => console.log('Master ETH balance', bal.toNumber()))
   deployer.then(() => ETH.balanceOf(seller))
     .then(bal => console.log('Seller ETH balance', bal.toNumber()))
   deployer.then(() => GNO.balanceOf(seller))
