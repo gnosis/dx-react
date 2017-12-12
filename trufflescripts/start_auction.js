a902c011c23ed1f7772a0c013d2861813092c9f8
David Sato <david.sato64@gmail.com>
2017-12-12 16:54:51 +0100

    edited argv args to take array of arguments and start an auction - defaults to certain values if omitted - tested with hacked contract

--- trufflescripts/start_auction.js
+++ trufflescripts/start_auction.js
@@ -1,6 +1,25 @@
 /* eslint no-console:0 */
-const DutchExchangeETHGNO = artifacts.require('./DutchExchangeETHGNO.sol')
+const DutchExchange = artifacts.require('DutchExchange')
+const TokenETH = artifacts.require('EtherToken')
+const TokenGNO = artifacts.require('TokenGNO')
+// const TokenTUL = artifacts.require('StandardToken')
+// const TokenOWL = artifacts.require('OWL')
+
 const { getTime, increaseTimeBy } = require('./utils')(web3)
+const argv = require('minimist')(process.argv.slice(4), { string: 'a' })
+
+/**
+ * truffle exec trufflescripts/start_auction.js
+ * give tokens from master
+ * @flags:
+ * sellToken          || 'eth'
+ * buyToken           || 'gno'
+ * sellAmount         ||  500
+ * buyAmount          ||  500
+ * -a <address>       to the given address
+ * --seller           to seller
+ * --buyer            to buyer
+ */
 
 /**
  * truffle exec trufflescripts/start_auction.js
@@ -11,12 +30,59 @@ const { getTime, increaseTimeBy } = require('./utils')(web3)
 const hour = 3600
 
 module.exports = async () => {
-  const dx = await DutchExchangeETHGNO.deployed()
-  const auctionStart = (await dx.auctionStart()).toNumber()
+  console.log(argv._)
+
+  const dx = await DutchExchange.deployed()
+  const eth = await TokenETH.deployed()
+  const gno = await TokenGNO.deployed()
+  // const tul = await TokenTUL.deployed()
+  // const owl = await TokenOWL.deployed()
+
+  let account
+  if (argv.a) account = argv.a
+  else if (argv.buyer) {
+    [, , account] = web3.eth.accounts
+  } else {
+    // set Seller as default account
+    [, account] = web3.eth.accounts
+  }
+
+  // Modify argv if necessary
+  const sellToken = argv._[0] === 'eth' ? eth : argv._[0] === 'gno' ? gno : eth
+  const buyToken = argv._[1] === 'gno' ? gno : argv._[1] === 'eth' ? eth : gno
+
+  console.log(`sellToken = ${await sellToken.symbol.call()} // buyToken = ${await buyToken.symbol.call()}`)
+
+  // Grab Deposited Token Balances in Auction (if any)
+  const balances = acct => Promise.all([
+    dx.balances(eth.address, acct),
+    dx.balances(gno.address, acct),
+  ]).then(res => res.map(bal => bal.toNumber()))
+
+  const [ethBalance, gnoBalance] = await balances(account)
+  console.log(`DX Ether Balance = ${ethBalance} && GNO Balance = ${gnoBalance}`)
+
+  try {
+    await sellToken.approve(dx.address, 10000, { from: account })
+    await buyToken.approve(dx.address, 10000, { from: account })
+    await dx.addTokenPair(
+      sellToken.address,
+      buyToken.address,
+      (argv._[2] || 500),
+      (argv._[3] || 500),
+      2,
+      1,
+      { from: account },
+    )
+  } catch (e) {
+    console.log(e)
+  }
+
+  const auctionStart = (await dx.auctionStarts.call(sellToken.address, buyToken.address)).toNumber()
   const now = getTime()
   const timeUntilStart = auctionStart - now
 
-  const auctionIndex = (await dx.auctionIndex()).toNumber()
+  const auctionIndex = (await dx.latestAuctionIndices.call(sellToken.address, buyToken.address)).toNumber()
 
   // auctionStart is in the future
   if (timeUntilStart > 0) {
