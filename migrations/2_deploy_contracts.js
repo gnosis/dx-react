2ecf41aa23ea32a95bdefdeae52bfdba1f2c2b5a
David Sato <david.sato64@gmail.com>
2018-01-02 11:29:24 +0100

    fixed Math deploy issue and cleaned up contract imports

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 /* eslint no-multi-spaces: 0, no-console: 0 */
 
-const Math = artifacts.require('Math')
+const Math2 = artifacts.require('Math')
 const DutchExchange = artifacts.require('DutchExchange')
 const EtherToken = artifacts.require('EtherToken')
 const PriceOracle = artifacts.require('PriceOracle')
@@ -11,12 +11,12 @@ const TokenTUL = artifacts.require('TokenTUL')
 
 module.exports = function deploy(deployer, networks, accounts) {
   let PriceOracleInstance
-  let TULInstance;
-  deployer.deploy(Math)
-  deployer.link(Math, [OWL, PriceOracle, DutchExchange, StandardToken, EtherToken, TokenGNO, TokenTUL])
+  // let TULInstance
+  deployer.deploy(Math2)
+  deployer.link(Math2, [OWL, PriceOracle, DutchExchange, StandardToken, EtherToken, TokenGNO, TokenTUL])
 
   deployer.deploy(EtherToken)
-    .then(() => deployer.deploy(TokenGNO, 10**19))
+    .then(() => deployer.deploy(TokenGNO, 10 ** 19))
     .then(() => deployer.deploy(TokenTUL, accounts[0], accounts[0]))
     .then(() => deployer.deploy(StandardToken))
     .then(() => deployer.deploy(PriceOracle, accounts[0], EtherToken.address))
@@ -25,7 +25,7 @@ module.exports = function deploy(deployer, networks, accounts) {
       PriceOracleInstance = p
       return deployer.deploy(OWL, TokenGNO.address /* ,PriceOracle.adress */, 0)
     })
-    
+
     // @dev DX Constructor creates exchange
     .then(() => deployer.deploy(
       DutchExchange,              // Contract Name
@@ -35,7 +35,7 @@ module.exports = function deploy(deployer, networks, accounts) {
       EtherToken.address,         // @param _ETH                - address of ETH ERC-20 token
       PriceOracle.address,        // @param _priceOracleAddress - address of priceOracle
       10000,
-      1000
+      1000,
     ))
     .then(() => {
       PriceOracleInstance.updateDutchExchange(DutchExchange.address, { from: accounts[0] })
--- test/dutchExchange.js
+++ test/dutchExchange.js
@@ -3,40 +3,40 @@ const { timestamp, blockNumber, assertRejects } = require('./utils')
 const { wait } = require('@digix/tempo')(web3)
 
 // > Import files
-const im = ['DutchExchange', 'EtherToken', 'PriceOracle', 'TokenGNO', 'StandardToken', 'OWL']
-for (let i = 0; i < im.length; i++) {
-  eval(`var ${im[i]} = artifacts.require('${im[i]}')`)
-}
-const Token = artifacts.require('./Token.sol')
+const [
+  DutchExchange,
+  EtherToken,
+  PriceOracle,
+  TokenGNO,
+  StandardToken,
+  Token,
+  OWL,
+] = ['DutchExchange', 'EtherToken', 'PriceOracle', 'TokenGNO', 'StandardToken', 'Token', 'OWL'].map(c => artifacts.require(c))
 
-// > Test VARS 
-var eth
+// > Test VARS
+let eth
 let gno
 let dx
 let oracle
 
 // > Other Variables
 // Variables must be at top so they are referencable
-const tokenPairs = [
+const tokenPairs = []
 
-]
+const approvedTokens = []
 
-const approvedTokens = [
-
-]
-
-contract('DutchExchange', async function(accounts) {
+contract('DutchExchange', async (accounts) => {
   it('sets up tests', async () => {
-  	await setupTest(accounts)
+    await setupTest(accounts)
   })
 
   for (let j = 0; j < 50; j++) {
-    it('transaction details:', async function() {
-      console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-      console.log('another transaction',);
-      console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-    	const t = await selectTransaction()
-    	console.log(t[0]);
+    it('transaction details:', async () => {
+      console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+      console.log('another transaction')
+      console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+      const t = await selectTransaction()
+      console.log(t[0])
       await wait(1800)
       await anotherTransaction(accounts, t)
     })
@@ -47,8 +47,8 @@ contract('DutchExchange', async function(accounts) {
 async function selectTransaction() {
   // const r = Math.floor(Math.random() * 6)
   const r = Math.floor(Math.random() * 2) + 2
-  let fn;
-  switch(r) {
+  let fn
+  switch (r) {
     case 0:
       fn = addTokenPair
       break
@@ -56,52 +56,50 @@ async function selectTransaction() {
       fn = updateApprovalOfToken
       break
     case 2:
-    	fn = postSellOrder
-    	break
+      fn = postSellOrder
+      break
     case 3:
-    	fn = postBuyOrder
-    	break
+      fn = postBuyOrder
+      break
     case 4:
-    	fn = claimSellerFunds
-    	break
+      fn = claimSellerFunds
+      break
     case 5:
-    	fn = claimBuyerFunds
-    	break
-     default:
+      fn = claimBuyerFunds
+      break
+    default:
   }
   if (r > 1) {
-   	const u = Math.floor(Math.random() * 5)
+    const u = Math.floor(Math.random() * 5)
 
-   	const tokenPair = tokenPairs[Math.floor(Math.random() * tokenPairs.length)]
-   	let [Ts, Tb] = tokenPair
-   	if (Math.floor(Math.random() * 2) == 1) [Ts, Tb] = [Tb, Ts]
+    const tokenPair = tokenPairs[Math.floor(Math.random() * tokenPairs.length)]
+    let [Ts, Tb] = tokenPair
+    if (Math.floor(Math.random() * 2) == 1) [Ts, Tb] = [Tb, Ts]
 
     const lAI = (await dx.getAuctionIndex(tokenPair[0], tokenPair[1])).toNumber()
 
     const aI = Math.floor(Math.random() * 3) - 1 + lAI
 
     const t = [
-		    `executing ${fn.name}(${Ts}, ${Tb}, ${aI})`,
-		    fn,
-		    Ts,
-		    Tb,
-		    u,
-		    aI
-	   ]
+      `executing ${fn.name}(${Ts}, ${Tb}, ${aI})`,
+      fn,
+      Ts,
+      Tb,
+      u,
+      aI,
+    ]
 
     if (r == 2 || r == 3) {
-    	// random amount
-    	t.push(10 ** 9)
-    	return t
-    } else {
-	    return t
+      // random amount
+      t.push(10 ** 9)
+      return t
     }
-  } else {
-  	return [
-  		fn.name,
-  		fn
-  	]
+    return t
   }
+  return [
+    fn.name,
+    fn,
+  ]
 }
 
 async function addTokenPair() {
@@ -111,92 +109,90 @@ async function updateApprovalOfToken() {
 
 }
 async function postSellOrder(Ts, Tb, u, aI, am) {
-	let expectToPass = true
+  let expectToPass = true
   let i = 0
   while (expectToPass && i < 2) {
     expectToPass = await postSellOrderConditions(i, Ts, Tb, u, aI, am)
     i++
   }
-	if (expectToPass) { console.log('successful',); await dx.postSellOrder(Ts, Tb, aI, am, { from: u }) }
-		else { console.log('rejected',); assertRejects(dx.postSellOrder(Ts, Tb, aI, am, { from: u })) }
+  if (expectToPass) { console.log('successful'); await dx.postSellOrder(Ts, Tb, aI, am, { from: u }) } else { console.log('rejected'); assertRejects(dx.postSellOrder(Ts, Tb, aI, am, { from: u })) }
 }
 async function postBuyOrder(Ts, Tb, u, aI, am) {
-	let expectToPass = true
-	let i = 0
-	while (expectToPass && i < 4) {
-		expectToPass = await postBuyOrderConditions(i, Ts, Tb, u, aI, am)
-		i++
-	}
-	if (expectToPass) { console.log('successful',); await dx.postBuyOrder(Ts, Tb, aI, am, { from: u }); }
-		else { console.log('rejected',); assertRejects( dx.postBuyOrder(Ts, Tb, aI, am, { from: u }) ) }
+  let expectToPass = true
+  let i = 0
+  while (expectToPass && i < 4) {
+    expectToPass = await postBuyOrderConditions(i, Ts, Tb, u, aI, am)
+    i++
+  }
+  if (expectToPass) { console.log('successful'); await dx.postBuyOrder(Ts, Tb, aI, am, { from: u }) } else { console.log('rejected'); assertRejects(dx.postBuyOrder(Ts, Tb, aI, am, { from: u })) }
 }
 async function claimSellerFunds(Ts, Tb, u, aI) {
-	await dx.claimSellerFunds(Ts, Tb, u, aI, { from: u })
+  await dx.claimSellerFunds(Ts, Tb, u, aI, { from: u })
 }
 async function claimBuyerFunds(Ts, Tb, u, aI) {
-	await dx.claimBuyerFunds(Ts, Tb, u, aI, { from: u })
+  await dx.claimBuyerFunds(Ts, Tb, u, aI, { from: u })
 }
 
 // > anotherTransaction()
-async function anotherTransaction (accounts, t) {
-	if (t.length > 2) {
-		// pSO & pBO are handled differently
-		if (t[1] == postSellOrder || t[1] == postBuyOrder) {
-			// find out if should be accepted or rejected
-			t[1](t[2], t[3], accounts[t[4]], t[5], t[6])
-		} 
-		// so are cSF & cBF
-		else {
-			// find out if should be accepted or rejected
-			t[1](t[2], t[3], accounts[t[4]], t[5])
-		}
-	}
+async function anotherTransaction(accounts, t) {
+  if (t.length > 2) {
+    // pSO & pBO are handled differently
+    if (t[1] == postSellOrder || t[1] == postBuyOrder) {
+      // find out if should be accepted or rejected
+      t[1](t[2], t[3], accounts[t[4]], t[5], t[6])
+    }
+    // so are cSF & cBF
+    else {
+      // find out if should be accepted or rejected
+      t[1](t[2], t[3], accounts[t[4]], t[5])
+    }
+  }
 }
 
 async function postSellOrderConditions(i, Ts, Tb, u, aI, am) {
   if (i == 0) {
     const bal = (await dx.balances(Ts, u)).toNumber()
-    if (am > bal) { console.log('failed at 1st case',); return false }
+    if (am > bal) { console.log('failed at 1st case'); return false }
   } else if (i == 1) {
     const lAI = (await dx.getAuctionIndex(Ts, Tb)).toNumber()
-    if (aI !== lAI) { console.log('failed at 2nd case',); return false }
+    if (aI !== lAI) { console.log('failed at 2nd case'); return false }
   }
 
   return true
 }
 
 async function postBuyOrderConditions(i, Ts, Tb, u, aI, am) {
-	// await doesn't work with switch()...
-	if (i == 0) {
-		const aS = (await dx.getAuctionStart(Ts, Tb))
-    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-    console.log('aS',aS);
-    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-    console.log('new Date', (new Date()).getTime() / 1000);
-    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-		if (aS > (new Date()).getTime() / 1000) { console.log('failed at 1st case',); return false }
-	} else if (i == 1) {
-		if (aI <= 0) { console.log('failed at 2nd case',); return false }
-	} else if (i == 2) {
-		const lAI = (await dx.getAuctionIndex(Ts, Tb)).toNumber()
-    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-    console.log('lAI',lAI);
-    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-		if (aI !== lAI) { console.log('failed at 3rd case',); return false }
-	} else if (i == 3) {
-		const cP = (await dx.closingPrices(Ts, Tb, aI)).map(x => x.toNumber())
-		console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-		console.log('cP',cP);
-		console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-		if (cP[0] !== 0) { console.log('failed at 4th case',); return false }
-	}
+  // await doesn't work with switch()...
+  if (i == 0) {
+    const aS = (await dx.getAuctionStart(Ts, Tb))
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    console.log('aS', aS)
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    console.log('new Date', (new Date()).getTime() / 1000)
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    if (aS > (new Date()).getTime() / 1000) { console.log('failed at 1st case'); return false }
+  } else if (i == 1) {
+    if (aI <= 0) { console.log('failed at 2nd case'); return false }
+  } else if (i == 2) {
+    const lAI = (await dx.getAuctionIndex(Ts, Tb)).toNumber()
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    console.log('lAI', lAI)
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    if (aI !== lAI) { console.log('failed at 3rd case'); return false }
+  } else if (i == 3) {
+    const cP = (await dx.closingPrices(Ts, Tb, aI)).map(x => x.toNumber())
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    console.log('cP', cP)
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    if (cP[0] !== 0) { console.log('failed at 4th case'); return false }
+  }
 
-	return true
+  return true
 }
 
 // > setupTest()
-async function setupTest (accounts) {
+async function setupTest(accounts) {
   gno = await TokenGNO.deployed()
   eth = await EtherToken.deployed()
   dx = await DutchExchange.deployed()
@@ -227,13 +223,13 @@ async function setupTest (accounts) {
 
   // add token Pair
   await dx.addTokenPair(
-	  eth.address,
-	  gno.address,
-	  10 ** 9,
-	  0,
-	  2,
-	  1,
-	  { from: accounts[1] },
+    eth.address,
+    gno.address,
+    10 ** 9,
+    0,
+    2,
+    1,
+    { from: accounts[1] },
   )
 
   tokenPairs.push([eth.address, gno.address])
