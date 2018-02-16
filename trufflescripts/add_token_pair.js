098d8eb2ab2b5c43a718b2c1d5cfd69211e7d3ad
david <david.sato64@gmail.com>
2018-02-16 11:47:09 +0100

    adding functionality to add different token pairs

--- trufflescripts/add_token_pair.js
+++ trufflescripts/add_token_pair.js
@@ -1,10 +1,11 @@
 /* eslint no-console:0, no-multi-spaces:0, prefer-destructuring:1 */
 
-const TokenETH = artifacts.require('./EtherToken.sol')
-const TokenGNO = artifacts.require('./TokenGNO.sol')
-const PriceOracle = artifacts.require('./PriceFeed.sol')
-const Medianizer = artifacts.require('./Medianizer.sol')
-const DutchExchange = artifacts.require('./DutchExchange.sol')
+const StandardToken = artifacts.require('StandardToken')
+const TokenETH = artifacts.require('EtherToken')
+const TokenGNO = artifacts.require('TokenGNO')
+const PriceOracle = artifacts.require('PriceFeed')
+const Medianizer = artifacts.require('Medianizer')
+const DutchExchange = artifacts.require('DutchExchange')
 const Proxy = artifacts.require('Proxy')
 
 const argv = require('minimist')(process.argv.slice(2), { string: 'a' })
@@ -18,6 +19,7 @@ const argv = require('minimist')(process.argv.slice(2), { string: 'a' })
  * -a <address>                 as the given address
  * --eth <number>               ETH tokens
  * --gno <number>               GNO tokens
+ * --pair <sellToken,buyToken>    token pair auction, eth,gno by default
  */
 
 module.exports = async () => {
@@ -26,8 +28,22 @@ module.exports = async () => {
   const dx = await DutchExchange.at(Proxy.address)
   const eth = await TokenETH.deployed()
   const gno = await TokenGNO.deployed()
+  const rdn = await TokenGNO.new(10000 * (10 ** 18))
+  const omg = await TokenGNO.new(10000 * (10 ** 18))
   const oracle = await PriceOracle.deployed()
   const medianizer = await Medianizer.deployed()
+
+  const availableTokens = {
+    eth,
+    gno,
+    rdn,
+    omg,
+  }
+
+  const [sell, buy] = argv.pair ? argv.pair.split(',') : ['eth', 'gno']
+
+  const sellToken = availableTokens[sell.toLowerCase()]
+  const buyToken = availableTokens[buy.toLowerCase()]
 
   const startingETH = web3.toWei(50, 'ether')
   const startingGNO = web3.toWei(50, 'ether')
@@ -38,28 +54,33 @@ module.exports = async () => {
     if (acct === accounts[0]) return
     eth.deposit({ from: acct, value: startingETH })
     eth.approve(dx.address, startingETH, { from: acct })
-    gno.transfer(acct, startingGNO, { from: accounts[0] })
-    gno.approve(dx.address, startingGNO, { from: acct })
+    if (sell === 'eth') {
+      buyToken.transfer(acct, startingGNO, { from: accounts[0] })
+      buyToken.approve(dx.address, startingGNO, { from: acct })
+    } else {
+      sellToken.transfer(acct, startingGNO, { from: accounts[0] })
+      sellToken.approve(dx.address, startingGNO, { from: acct })
+    }
   }))
   // Deposit depends on ABOVE finishing first... so run here
   await Promise.all(accounts.map((acct) => {
     if (acct === accounts[0]) return
-    dx.deposit(eth.address, startingETH, { from: acct })
-    dx.deposit(gno.address, startingGNO, { from: acct })
+    dx.deposit(sellToken.address, startingETH, { from: acct })
+    dx.deposit(buyToken.address, startingGNO, { from: acct })
   }))
 
   await oracle.post(ethUSDPrice, 1516168838 * 2, medianizer.address, { from: accounts[0] })
 
   console.log('Threshold new token pair == ', (await dx.thresholdNewTokenPair.call()).toNumber() / (10 ** 18))
-  console.log('ETHER = ', (await dx.balances.call(eth.address, accounts[1])).toNumber() / (10 ** 18))
-  console.log('GNO = ', (await dx.balances.call(gno.address, accounts[1])).toNumber() / (10 ** 18))
+  console.log('Sell Token = ', sell, '|| BAL == ', (await dx.balances.call(sellToken.address, accounts[1])).toNumber() / (10 ** 18))
+  console.log('Buy Token = ', buy, '|| BAL == ', (await dx.balances.call(buyToken.address, accounts[1])).toNumber() / (10 ** 18))
   console.log('FundingUSD == ', startingETH * ethUSDPrice)
-  console.log('Auction Index == ', (await dx.getAuctionIndex.call(eth.address, gno.address)).toNumber())
+  console.log('Auction Index == ', (await dx.getAuctionIndex.call(sellToken.address, buyToken.address)).toNumber())
 
 
   await dx.addTokenPair(
-    TokenETH.address,                            // -----> SellToken Address
-    TokenGNO.address,                           // -----> BuyToken Address
+    sellToken.address,                            // -----> SellToken Address
+    buyToken.address,                           // -----> BuyToken Address
     web3.toWei(10, 'ether'),                   // -----> token1Funding
     0,                                        // -----> token2Funding
     2,                                       // -----> closingPriceNum
