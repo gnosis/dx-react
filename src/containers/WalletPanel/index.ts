eeb96b492a79a74d010b85c1dbd126d0e23d906a
david <david.sato64@gmail.com>
2018-02-22 12:51:54 +0100

    1. Cleaned up WalletPanel container logic to pass only necessary fn 2. WalletPanel component only receives neccessary fn getTokenAllowance   a. opens Modal now instead of postSellOrder

--- src/containers/WalletPanel/index.ts
+++ src/containers/WalletPanel/index.ts
@@ -1,36 +1,5 @@
-import { connect, Dispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import WalletPanel, { WalletPanelProps } from 'components/WalletPanel'
-// import { State } from 'types'
-import { submitSellOrder } from 'actions'
+import { getTokenAllowance, openModal } from 'actions'
 
-const mapStateToProps = () => ({
-  // TODO: get address from store, populated by contract addresses
-  auctionAddress: '0x03494929349594',
-})
-
-const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
-  
-  submitOrder(proceedTo: string, modalName: string) {
-    console.log('Submitting order')
-
-    dispatch(submitSellOrder(proceedTo, modalName))
-  },
-})
-
-const mergeProps = (
-  stateProps: Partial<WalletPanelProps>,
-  dispatchProps: { submitOrder: Function },
-  ownProps: Partial<WalletPanelProps>,
-) => ({
-  ...stateProps,
-  submitOrder(e: MouseEvent) {
-    // don't go to /auction/0x03494929349594 just yet
-    e.preventDefault()
-
-    dispatchProps.submitOrder(`/auction/${stateProps.auctionAddress}`, 'TransactionModal')
-  },
-  ...ownProps,
-})
-
-
-export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(WalletPanel)
+export default connect<WalletPanelProps>(undefined, { getTokenAllowance, openModal })(WalletPanel)
