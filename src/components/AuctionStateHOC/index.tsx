3e3f02e5412750590ad5a5dd373f60f876eef8d7
Velenir <Velenir@users.noreply.github.com>
2018-03-02 14:55:02 +0300

    added one more valid auction check, helpful state display and fixed types

--- src/components/AuctionStateHOC/index.tsx
+++ src/components/AuctionStateHOC/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TokenCode, Balance } from 'types'
+import { TokenCode } from 'types'
 import { BigNumber } from 'bignumber.js'
 import { code2tokenMap, AuctionStatus } from 'globals'
 // import { promisedDutchX } from 'api/dutchx'
@@ -30,7 +30,7 @@ export interface AuctionStateState {
   status: string,
   sell: TokenCode,
   buy: TokenCode,
-  price: [BigNumber, BigNumber],
+  price: number[],
   timeToCompletion: number,
   userSelling: number,
   userGetting:  number,
@@ -39,7 +39,7 @@ export interface AuctionStateState {
 
 interface AuctionStatusArgs {
   closingPrice: [BigNumber, BigNumber],
-  index: string,
+  index: number,
   currentAuctionIndex: BigNumber,
   auctionStart: BigNumber,
   now: number,
@@ -60,7 +60,7 @@ const getAuctionStatus = ({
   if (!price[1].equals(0)) return AuctionStatus.ACTIVE
 }
 
-export default (Component: React.ClassType<any, any, any>) => {
+export default (Component: React.ClassType<any, any, any>): React.ClassType<any, any, any> => {
   return class extends React.Component<AuctionStateProps, AuctionStateState> {
 
     async componentDidMount() {
@@ -69,11 +69,17 @@ export default (Component: React.ClassType<any, any, any>) => {
       } else {
         console.warn('invalid auction')
       }
-      
+      (window as any).updateAuctionState = this.updateAuctionState.bind(this)
     }
 
     async updateAuctionState() {
-      const { sell, buy, index } = this.props.match.params
+      const { sell, buy, index: indexParam } = this.props.match.params
+      const index = +indexParam
+
+      if (Number.isNaN(index) || index < 0 || !Number.isInteger(index)) {
+        console.warn(`wrong index format: ${indexParam}`)
+        return
+      }
 
       if (!code2tokenMap[sell] || !code2tokenMap[buy]) {
         console.warn(`${sell}->${buy} auction isn't supported in Frontend UI`)
@@ -83,7 +89,7 @@ export default (Component: React.ClassType<any, any, any>) => {
       const pair = { sell, buy }
 
       const currentAuctionIndex = await getLatestAuctionIndex(pair)
-      console.log('currentAuctionIndex: ', currentAuctionIndex)
+      console.log('currentAuctionIndex: ', currentAuctionIndex.toNumber())
 
       if (currentAuctionIndex.equals(0)) {
         console.warn(`${sell}->${buy} auction hasn't run once yet`)
@@ -91,15 +97,15 @@ export default (Component: React.ClassType<any, any, any>) => {
         return
       }
 
-      if (!currentAuctionIndex.lessThan(index)) {
-        console.warn(`auction index ${index} haven't run yet,\ncurrent index = ${currentAuctionIndex}`)
+      if (!currentAuctionIndex.lessThanOrEqualTo(index)) {
+        console.warn(`auction index ${index} hasn't run yet,\ncurrent index = ${currentAuctionIndex.toNumber()}`)
         return
       }
 
       const promisedAccount = getCurrentAccount()
       const [closingPrice, price, auctionStart, now] = await Promise.all([
-        getClosingPrice(pair, +index),
-        getPrice(pair, +index),
+        getClosingPrice(pair, index),
+        getPrice(pair, index),
         getAuctionStart(pair),
         getTime(),
       ])
@@ -114,12 +120,12 @@ export default (Component: React.ClassType<any, any, any>) => {
       })
 
       const account = await promisedAccount
-      const sellerBalance = await getSellerBalance(pair, +index, account)
+      const sellerBalance = await getSellerBalance(pair, index, account)
 
       const userGetting = sellerBalance.mul(price[0]).div(price[1]).toNumber()
 
       const userCanClaim = sellerBalance.greaterThan(0) && closingPrice[0].greaterThan(0) ?
-        (await getUnclaimedSellerFunds(pair, +index, account)).toNumber() : 0
+        (await getUnclaimedSellerFunds(pair, index, account)).toNumber() : 0
       
       const timeToCompletion = status === AuctionStatus.ACTIVE ? auctionStart.plus(86500 - now).toNumber() : 0
       
@@ -129,7 +135,7 @@ export default (Component: React.ClassType<any, any, any>) => {
         status,
         sell,
         buy,
-        price,
+        price: price.map(n => n.toNumber()),
         timeToCompletion,
         userSelling: sellerBalance.toNumber(),
         userGetting,
@@ -141,7 +147,13 @@ export default (Component: React.ClassType<any, any, any>) => {
 
     render() {
       // TODO: redirect home on invalid auction or something
-      return this.state.status ? <Component {...this.props} {...this.state}/> :
+      return this.state && this.state.status ? 
+        <div>
+          <pre style={{ position: 'fixed', zIndex: 2, opacity: 0.9 }}>
+            {JSON.stringify(this.state, null, 2)}
+          </pre>
+          <Component {...this.props} {...this.state}/> :
+        </div> :
         <h3>Invalid auction</h3>
     }
   }
