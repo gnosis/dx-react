b773c30dbb76e4cde00ff4a6960141530a9850f5
Velenir <velenir@gmail.com>
2018-03-06 16:21:23 +0300

    added TokenClaimingHOC for claimSellerFunds functionality

---
+++ src/components/TokenClaimingHOC/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { claimSellerFunds } from 'api'
+import { TokenCode } from 'types'
+
+export interface TokenClaimingProps {
+  completed: boolean,
+  sellToken: TokenCode,
+  buyToken: TokenCode,
+  index: number,
+  buyAmount: number,
+  account: string,
+}
+
+export interface TokenClaimingState {
+  isClaiming: boolean,
+  error?: string,
+}
+
+export default (Component: React.ClassType<any, any, any>): React.ClassType<TokenClaimingProps, any, any> => {
+  return class TokenClaiming extends React.Component<TokenClaimingProps, TokenClaimingState> {
+    state = { isClaiming: false }
+
+    claimTokens = async () => {
+      const { completed, buyAmount, account, sellToken: sell, buyToken: buy, index } = this.props
+      // don't claim anything if auction isn't completed or nothing to claim
+      if (!completed || buyAmount <= 0) return
+
+      // indicate that claiming is in progress
+      // can be used to disallow multiple simultaneous claimRequests or just for indication
+      this.setState({
+        isClaiming: true,
+        // clear whatever error might be left from previous requests
+        error: null,
+      })
+
+      try {
+        // start claimFunds request
+        await claimSellerFunds({ sell, buy }, index, account)
+        // if succeeds change isClaiming state
+        this.setState({
+          isClaiming: false,
+        })      
+      } catch (error) {
+        // if fails change isClaiming state and add error
+        console.warn('Error claiming tokens', error)
+        this.setState({
+          isClaiming: false,
+          error: error.message,
+        })
+      }
+
+    }
+
+    render() {
+      return <Component {...this.props} {...this.state} claimTokens={this.claimTokens}/>
+    }
+  }
+}
