c83b28b77dce6e116092bb601eea42efd3d2d7a5
david <david.sato64@gmail.com>
2018-04-11 12:59:06 +0200

    updated components to take new data structure of ongoingAUctions, changed types, fixed minor CSS issue + created MenuFeeBalance container

--- src/components/MenuFeeBalance/index.tsx
+++ src/components/MenuFeeBalance/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
-
+import { Balance } from 'types'
 // Hook in props:
 // MGN amount found via TokenMGN.balanceOf(user.address)
 // Fee level should be estimated in DX via MGN.totalSupply() vs Users supply
 // Fee percentages can be found via Dom's numbers
-const MenuFeeBalance = () =>
-  <div className="menuFeeBalance">
-    <p>MGN <strong>130455.549</strong></p>
-    <p>Fee level <strong>0.15%</strong></p>
-  </div>
+
+export interface MenuFeeBalanceProps {
+  feeRatio: number,
+  mgnSupply: Balance
+}
+
+const MenuFeeBalance = ({ feeRatio, mgnSupply }: MenuFeeBalanceProps) =>
+  mgnSupply && feeRatio
+    ?
+    <div className="menuFeeBalance">
+      <p>MGN <strong>{mgnSupply}</strong></p>
+      <p>Fee level <strong>{`${feeRatio * 100}%`}</strong></p>
+    </div>
+    :
+    <div className="menuFeeBalance"><p>Loading Fee Info ... </p></div>
 
 export default MenuFeeBalance
