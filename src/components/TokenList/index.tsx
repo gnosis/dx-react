89ba75756f0902c82885bff92d5921a3764d1982
Velenir <velenir@gmail.com>
2018-04-13 18:58:59 +0300

    DX-232 give each TokenItem in TokenList generatesMGN prop based on approvedTokens

--- src/components/TokenList/index.tsx
+++ src/components/TokenList/index.tsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import TokenItem, { TokenItemProps } from '../TokenItem'
 import { code2tokenMap } from 'globals'
-import { TokenCode, TokenBalances } from 'types'
+import { TokenCode, TokenBalances, State } from 'types'
 
 
 interface TokenListProps {
   tokens: TokenCode[],
   balances: TokenBalances,
-  onTokenClick(props: TokenItemProps): any
+  onTokenClick(props: TokenItemProps): any,
+  approvedTokens: State['approvedTokens'],
 }
 
-const TokenList: React.SFC<TokenListProps> = ({ tokens, balances, onTokenClick }) => (
+const TokenList: React.SFC<TokenListProps> = ({ tokens, balances, onTokenClick, approvedTokens }) => (
   <div className="tokenList">
     {tokens.map(code =>
-      <TokenItem name={code2tokenMap[code]} code={code} balance={balances[code]} key={code} onClick={onTokenClick} />)}
+      <TokenItem
+        name={code2tokenMap[code]}
+        code={code}
+        balance={balances[code]}
+        key={code}
+        onClick={onTokenClick}
+        generatesMGN={approvedTokens.has(code)}
+      />,
+    )}
   </div>
 )
 
