b9c1f5ae903aca5be18e4e63e4f54bd66588970b
Velenir <velenir@gmail.com>
2018-04-13 18:30:38 +0300

    pass whether pair generates MGN to OrderPanel

--- src/containers/OrderPanel/index.ts
+++ src/containers/OrderPanel/index.ts
@@ -2,11 +2,19 @@ import { connect } from 'react-redux'
 import OrderPanel from 'components/OrderPanel'
 import { State } from 'types'
 
-const mapStateToProps = ({ tokenPair: { sell, buy, sellAmount } }: State) => ({
-  sellToken: sell,
-  buyToken: buy,
-  sellAmount,
-})
+const isTokenApproved = ({ approvedTokens, tokenPair: { sell, buy } }: State) =>
+  approvedTokens.has(sell) && approvedTokens.has(buy)
+
+const mapStateToProps = (state: State) => {
+  const { tokenPair: { sell, buy, sellAmount } } = state
+
+  return {
+    sellToken: sell,
+    buyToken: buy,
+    sellAmount,
+    generatesMGN: isTokenApproved(state),
+  }
+}
 
 
 export default connect(mapStateToProps)(OrderPanel)
