3fe55e0145bc309c783d34a9801d65a1a8ceccd7
Velenir <velenir@gmail.com>
2018-04-17 17:16:14 +0300

    memoizing token lists combinations

--- src/reducers/tokenList.ts
+++ src/reducers/tokenList.ts
@@ -2,24 +2,43 @@ import { handleActions } from 'redux-actions'
 
 import { setDefaultTokenList, setCustomTokenList } from 'actions'
 import { setTokenListType } from 'containers/TokenUpload'
+import { DefaultTokenObject } from 'api/types'
+
+import { createSelector } from 'reselect'
+
+const makeASet = createSelector(
+  (state: any, action: any) => action.payload.defaultTokenList || state.defaultTokenList,
+  list => new Set(list.map((t: DefaultTokenObject) => t.address)),
+)
+const copyList = createSelector(
+  (state: any, action: any) => action.payload.defaultTokenList || state.defaultTokenList,
+  list => list.slice(),
+)
+
+const combine = createSelector(
+  makeASet,
+  copyList,
+  (state, action) => action.payload.customTokenList || state.customTokenList,
+  (set, list, list2) => {
+    for (const token of list2) {
+      if (!set.has(token.address)) list.push(token)
+    }
+  
+    return list
+  },
+)
 
 export default handleActions(
   {
     [setDefaultTokenList.toString()]: (state: any, action: any) => ({
       ...state,
       defaultTokenList: action.payload.defaultTokenList,
-      combinedTokenList: [
-        ...state.customTokenList,
-        ...action.payload.defaultTokenList,
-      ],
+      combinedTokenList: combine(state, action),
     }),
     [setCustomTokenList.toString()]: (state: any, action: any) => ({
       ...state,
       customTokenList: action.payload.customTokenList,
-      combinedTokenList: [
-        ...state.defaultTokenList,
-        ...action.payload.customTokenList,
-      ],
+      combinedTokenList: combine(state, action),
     }),
     [setTokenListType.toString()]: (state: any, action: any) => {
       const { type } = action.payload
