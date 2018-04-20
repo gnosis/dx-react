6d8c4f994448c446a5b1682990126ab0e7504d23
david <david.sato64@gmail.com>
2018-04-20 14:53:46 +0200

    Changed Reducer logic to work with Code => Address changes

--- src/reducers/tokenBalances.ts
+++ src/reducers/tokenBalances.ts
@@ -8,8 +8,8 @@ import { setTokenBalance } from 'actions/tokenBalances'
 
 // TODO: fill in state with dispatch in or around getDefaultTokens
 const initialState: TokenBalances = {
+  '0xcfeb869f69431e42cdb54a4f4f105c19c080a601': '0',
   '0x254dffcd3277c0b1660f6d42efbb754edababc2b': '0',
-  '0xc89ce4735882c9f0f0fe26686c53074e09b0d550': '0',
 }
 
 // TODO: fill in when we have actions + fill in rest of Tokens + proper typing
@@ -17,7 +17,7 @@ export default handleActions<TokenBalances>(
   {
     [setTokenBalance.toString()]: (state: any, action: any) => ({
       ...state,
-      [action.payload.tokenName]: action.payload.balance,
+      [action.payload.address]: action.payload.balance,
     }),
   },
   // zeroBalance
