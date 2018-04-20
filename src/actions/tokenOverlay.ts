776451d6e204c6319d0774334e241eafa3ac7279
Velenir <velenir@gmail.com>
2018-04-20 18:32:18 +0300

    changed selectTokenPairAndRatioPair to swap Tokens if sellToken is picked for buyToken position (in open TokenOverlay)

--- src/actions/tokenOverlay.ts
+++ src/actions/tokenOverlay.ts
@@ -3,6 +3,7 @@ import { TokenMod, TokenPair } from 'types'
 
 import { closingPrice } from 'api/'
 import { setClosingPrice } from 'actions/ratioPairs'
+import { swapTokensInAPair } from 'actions/tokenPair'
 import { DefaultTokenObject } from 'api/types'
 
 export const openOverlay = createAction<{ mod: TokenMod }>('OPEN_OVERLAY')
@@ -10,10 +11,18 @@ export const closeOverlay = createAction<void>('CLOSE_OVERLAY', () => { })
 export const selectTokenAndCloseOverlay = createAction<{ mod: TokenMod, token: DefaultTokenObject }>(
   'SELECT_TOKEN_AND_CLOSE_OVERLAY')
 
-export const selectTokenPairAndRatioPair = (props: any) => async (dispatch: Function, getState: any) => {
+type PropsType = { mod: TokenMod, token: DefaultTokenObject }
+
+export const selectTokenPairAndRatioPair = (props: PropsType) => async (dispatch: Function, getState: any) => {
   const { tokenPair } = getState()
   const { token, mod } = props
+  console.log('props: ', props)
   const { sell, buy }: TokenPair = { ...tokenPair, [mod]: token }
+
+  if (sell.address === buy.address) {
+    dispatch(swapTokensInAPair())
+    return dispatch(closeOverlay())
+  }
 
   try {
     const price = (await closingPrice({ sell, buy })).toString()
