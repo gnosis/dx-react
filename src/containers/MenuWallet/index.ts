dc4665d90bebd3f40c87d71009f01ae638bfbf1a
david <david.sato64@gmail.com>
2018-04-20 14:56:31 +0200

    Code => Address change - added AddressToSymbol reduce function to grab addresses to get token codes + changed in Container

--- src/containers/MenuWallet/index.ts
+++ src/containers/MenuWallet/index.ts
@@ -4,10 +4,19 @@ import { State } from 'types'
 
 import { MenuWallet, WalletProps } from 'components/MenuWallet'
 
-const mapStateToProps = (state: State) => ({
-  account: getAccount(state),
-  balance: getCurrentBalance(state),
-  tokens: state.tokenBalances,
-})
+const mapStateToProps = (state: State) => {
+  // TODO: reselect
+  const tokenList = state.tokenList.type === 'DEFAULT' ? state.tokenList.defaultTokenList : state.tokenList.combinedTokenList
+  const mapTokenListToNames = tokenList.reduce((acc: {}, tok: any) => {
+    acc[tok.address] = tok.symbol || tok.name || tok.address | 'Unknown Token'
+    return acc
+  }, {})
+  return {
+    account: getAccount(state),
+    addressToSymbol: mapTokenListToNames,
+    balance: getCurrentBalance(state),
+    tokens: state.tokenBalances,
+  }
+}
 
 export default connect<WalletProps>(mapStateToProps)(MenuWallet)
