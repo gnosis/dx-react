8c6e2e26b8da2d31b26258457138966a407ae8e3
david <david.sato64@gmail.com>
2018-04-24 18:30:22 +0200

    change tokenBalance action to use BN and reducer to be simpler w/initialState

--- src/actions/tokenBalances.ts
+++ src/actions/tokenBalances.ts
@@ -1,3 +1,4 @@
 import { createAction } from 'redux-actions'
+import { BigNumber } from 'types'
 
-export const setTokenBalance = createAction<{address: string, balance: string}>('SET_TOKEN_BALANCE')
+export const setTokenBalance = createAction<{address: string, balance: BigNumber}>('SET_TOKEN_BALANCE')
