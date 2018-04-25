f70503c65ef1cbd4a95797d66ceccc4af4c4862d
david <david.sato64@gmail.com>
2018-04-25 15:57:55 +0200

    created Modal scss file and moved scss from buttons.scss + removed js CSS in Modals.tsx + added token symbol/name logic incase none present

--- src/components/Modals/index.tsx
+++ src/components/Modals/index.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties } from 'react'
+import React from 'react'
 import { Modal } from 'types'
 import { closeModal, approveAndPostSellOrder } from 'actions'
 
@@ -18,76 +18,61 @@ interface BlockModalProps extends TransactionModalProps {
   disabledReason: string,
 }
 
-const tempParentDiv: CSSProperties = {
-  display: 'flex',
-  flexFlow: 'column nowrap',
-  alignItems: 'center', justifyContent: 'center',
-  width: '100%',
-}
-
-const tempButtonDiv: CSSProperties = {
-  display: 'flex',
-  flexFlow: 'row nowrap',
-  justifyContent: 'center', alignItems: 'center',
-  padding: 10,
-  width: '75%',
-}
-
-const tempDivStyle: CSSProperties = {
-  position: 'fixed', 
-  display: 'flex', flexFlow: 'column', justifyContent: 'center', alignItems: 'center',
-
-  color: 'white',
-  
-  height: '100vh', width: '100vw',
-  textAlign: 'center',
-}
-
-export const TransactionModal: React.SFC<TransactionModalProps> = ({ 
+export const TransactionModal: React.SFC<TransactionModalProps> = ({
   modalProps: {
-    header, 
+    header,
     body,
+    txData,
     button,
     error,
-  }, 
+  },
   activeProvider,
   closeModal,
 }) =>
-  <div style={tempDivStyle}>
-    <h1 className="modalH1">{header || 'Approving Tokens and Transferring'}</h1>
-    <p className="modalP">
+  <div className="modalDivStyle">
+    <h1>{header || 'Approving Tokens and Transferring'}</h1>
+    <p className="modalHeaderDescriptor">
       { body || `Please check your ${activeProvider || 'Provider'} notifications in extensions bar of your browser.` }
     </p>
-    {error && 
+    {txData && <div className="modalTXDataDiv">
+      <ul>
+        <li>{`Sell Amount:    ${txData.sellAmount}`}</li>
+        {(txData.tokenA.symbol || txData.tokenA.symbol) &&
+          <li>{`Token:          ${txData.tokenA.symbol || txData.tokenA.name} [${txData.tokenA.name || txData.tokenA.symbol}]`}</li>
+        }
+        <li>{`Token Address:  ${txData.tokenA.address}`}</li>
+      </ul>
+    </div>}
+    {error &&
     <p className="modalError">
       {`${error.slice(0, 300)}...`}
     </p>}
-    {button && 
-      <button
-        className="modalButton"
-        onClick={closeModal}
-      >
-        CLOSE
-      </button>
+    {button &&
+    <button
+      className="modalButton"
+      onClick={closeModal}
+    >
+      CLOSE
+    </button>
     }
   </div>
 
 export const ApprovalModal: React.SFC<ApprovalModalProps> = ({
   modalProps: {
-    header, 
+    header,
     body,
-  }, 
+  },
   activeProvider,
   approveAndPostSellOrder,
-}) => 
-  <div style={tempDivStyle}>
+}) =>
+  <div className="modalDivStyle">
     <h1 className="modalH1">{header || 'Please choose Token Approval amount'}</h1>
     <p className="modalP">
       { body || `Please check your ${activeProvider || 'Provider'} notifications in extensions bar of your browser.` }
     </p>
-    <div style={tempParentDiv}>
-      <div style={tempButtonDiv}>
-        <button 
+    <div className="modalParentDiv">
+      <div className="modalButtonDiv">
+        <button
           className="modalButton"
           onClick={() => approveAndPostSellOrder('MAX')}
           >
@@ -97,8 +82,8 @@ export const ApprovalModal: React.SFC<ApprovalModalProps> = ({
           Choose this option to stop seeing this prompt and only sign 1 transaction per sell order
         </p>
       </div>
-      <div style={tempButtonDiv}>  
-        <button 
+      <div className="modalButtonDiv">
+        <button
           className="modalButton"
           onClick={() => approveAndPostSellOrder('MIN')}
           >
@@ -107,7 +92,7 @@ export const ApprovalModal: React.SFC<ApprovalModalProps> = ({
         <p className="modalButtonDescription">
           Choose this option to require signing 2 transactions for each sell order
         </p>
-      </div>  
+      </div>
     </div>
   </div>
 
@@ -115,9 +100,9 @@ const disabledReasons = {
   geoblock: 'The Dutch Exchange is not available in your country',
 }
 
-export const BlockModal: React.SFC<BlockModalProps> = ({ 
-  disabledReason, 
+export const BlockModal: React.SFC<BlockModalProps> = ({
+  disabledReason,
 }) =>
-  <div style={tempDivStyle}>
+  <div className="modalDivStyle">
     <h1 className="modalH1">{disabledReasons[disabledReason]}</h1>
   </div>
