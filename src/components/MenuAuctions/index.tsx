ac18d0ed57af4b57e60a8a844f5334aa89935eb7
david <david.sato64@gmail.com>
2018-05-05 15:23:13 +0200

    Added Link to MenuAuctions to allow moving to AuctionStatus screen on click. Passed in ClaimSellerFundsFromSeveral on claim button clicking // types

--- src/components/MenuAuctions/index.tsx
+++ src/components/MenuAuctions/index.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react'
 import { OngoingAuctions } from 'types'
+import { Link } from 'react-router-dom'
+import { DefaultTokenObject } from 'api/types'
 
 export interface MenuAuctionProps {
-  name?: string,
-  ongoingAuctions: OngoingAuctions
+  name?: string;
+  ongoingAuctions: OngoingAuctions;
+  claimSellerFundsFromSeveral(sell: Partial<DefaultTokenObject>, buy: Partial<DefaultTokenObject>, indices?: number): any;
 }
 
 export const MenuAuctions: React.SFC<MenuAuctionProps> = ({
   name = 'YOUR AUCTIONS',
   ongoingAuctions,
+  claimSellerFundsFromSeveral,
 }) => (
     <div className="menuAuctions"><img src={require('assets/auction.svg')} />
       {name}
@@ -26,13 +30,17 @@ export const MenuAuctions: React.SFC<MenuAuctionProps> = ({
               {ongoingAuctions.map(
                 (auction, i) =>
                   <tr key={`${auction.sell.address}-${auction.buy.address}-${i}`}>
-                    <td>{`${auction.sell.symbol}/${auction.buy.symbol}`}</td>
+                    <td>
+                      <Link to={`/auction/${auction.sell.symbol}-${auction.buy.symbol}-${auction.indices[auction.indices.length - 1]}`}>
+                        {`${auction.sell.symbol}/${auction.buy.symbol}`}
+                      </ Link>
+                    </td>
                     {/* <td>{`${auction.indices[auction.indices.length - 1]}`}</td> */}
                     <td>
                       <p>{`${auction.balancePerIndex[auction.balancePerIndex.length - 1] || 'N/A'} ${auction.sell.symbol}`}</p>
                       <p>{`${auction.balancePerIndexInverse[auction.balancePerIndexInverse.length - 1] || 0} ${auction.buy.symbol}`}</p>
                     </td>
-                    {auction.claim && <td><img src={require('assets/claim.svg')} /></td>}
+                    {auction.claim && <td onClick={() => claimSellerFundsFromSeveral(auction.sell, auction.buy)}><img src={require('assets/claim.svg')} /></td>}
                   </tr>,
               )}
             </tbody>
