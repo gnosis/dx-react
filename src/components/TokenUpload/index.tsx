9b30df87e6a9591659823f9ac503fb9868c12579
david <david.sato64@gmail.com>
2018-05-09 21:36:31 +0200

    Changed logic / presentation of TokenUpload
    1. Using Loader to conditionally render TokenUpload based on HOC internal state
    2. changed wording

--- src/components/TokenUpload/index.tsx
+++ src/components/TokenUpload/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ButtonCTA from 'components/ButtonCTA'
+import Loader from 'components/Loader'
 
 import IPFSHOC from 'components/IPFSHOC'
 import { DefaultTokenObject } from 'api/types'
@@ -9,6 +10,7 @@ interface TokenUploadProps {
   handleGrabFromIPFS(): void;
   setTokenListType({}): void;
   customTokenList: DefaultTokenObject[];
+  pullingData: boolean;
 
   // IPFS
   potentiallyValidHash?: boolean;
@@ -17,45 +19,55 @@ interface TokenUploadProps {
   json?: Object;
   oFile?: File;
 }
-// TODO: add link URL in line 26
+// TODO: add link URL in line 45
 const TokenUpload = ({
   customTokenList,
   potentiallyValidHash,
   handleFileHashInput,
   handleGrabFromIPFS,
   setTokenListType,
+  fileHash,
+  pullingData,
 }: TokenUploadProps) => {
   return (
-    <div className="tokenUpload">
-      <span className="icon-tokenUpload" />
-      <h2>Download Token List</h2>
+    <Loader
+      hasData={!pullingData}
+      message={`Checking IPFS for hash: ${fileHash}...`}
+      render={() =>
+        <div className="tokenUpload">
+          <span className="icon-tokenUpload" />
+          <h2>Download Token List</h2>
 
-      <p>
-        Download a list of tokens to be added and start trading.
-        JSON formatted files are supported with a max filesize of 1MB.
-      <br />
-        Read our <a href="#">FAQ</a> on how this works.
-      </p>
+          <p>
+            Download a list of tokens to be added and start trading.
+            JSON formatted files are supported with a max filesize of 1MB.
+          <br />
+            Read our <a href="#">FAQ</a> on how this works.
+          </p>
 
-      <form>
-        <input
-          className="inputText"
-          name="tokenListHash"
-          onChange={handleFileHashInput}
-          type="text"
-        />
-        <ButtonCTA
-          className={potentiallyValidHash ? 'blue' : 'buttonCTA-disabled'}
-          id="ipfsDownload"
-          onClick={handleGrabFromIPFS}
-        >
-          {potentiallyValidHash ? 'Download' : 'Enter IPFS Hash'}
-        </ ButtonCTA>
-      </form>
+          <form>
+            <input
+              className="inputText"
+              name="tokenListHash"
+              onChange={handleFileHashInput}
+              type="text"
+              value={fileHash}
+            />
 
-      {customTokenList.length > 0 && <a className="showTokenUpload" onClick={(e) => (e.preventDefault(), setTokenListType({ type: 'CUSTOM' }))}>Use pre-existing custom token list</a>}
-      <a className="showTokenUpload" onClick={(e) => (e.preventDefault(), setTokenListType({ type: 'DEFAULT' }))}>Use default token list</a>
-    </div>
+            <ButtonCTA
+              className={potentiallyValidHash ? 'blue' : 'buttonCTA-disabled'}
+              id="ipfsDownload"
+              onClick={handleGrabFromIPFS}
+            >
+              {potentiallyValidHash ? 'Download' : 'Enter Hash'}
+            </ ButtonCTA>
+          </form>
+
+          {customTokenList.length > 0 && <a className="showTokenUpload" onClick={(e) => (e.preventDefault(), setTokenListType({ type: 'CUSTOM' }))}>Use pre-existing custom token list</a>}
+          <a className="showTokenUpload" onClick={(e) => (e.preventDefault(), setTokenListType({ type: 'DEFAULT' }))}>Use default token list</a>
+        </div>
+      }
+    />
   )
 }
 
