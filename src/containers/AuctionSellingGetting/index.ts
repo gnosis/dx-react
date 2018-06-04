ab05bcdc84e0acfa852d70baa48b101d0fdff3e2
david <david.sato64@gmail.com>
2018-06-04 17:08:12 +0200

    passed computed lastPrice (closingPrice) into necessarry containers

--- src/containers/AuctionSellingGetting/index.ts
+++ src/containers/AuctionSellingGetting/index.ts
@@ -1,13 +1,12 @@
 import { connect } from 'react-redux'
 import { setSellTokenAmount } from 'actions'
-import { findRatioPair } from 'selectors/ratioPairs'
 
 import { State } from 'types'
 import AuctionSellingGetting, { AuctionSellingGettingProps } from 'components/AuctionSellingGetting'
 
 const mapState = (state: State) => {
   // TODO: always have some price for every pair in RatioPairs
-  const { sell, buy, price } = findRatioPair(state) || Object.assign({ price: 2 }, state.tokenPair)
+  const { sell, buy, lastPrice: price } = state.tokenPair
   const { [sell.address]: sellTokenBalance } = state.tokenBalances
   const { sellAmount } = state.tokenPair
   const maxSellAmount = sellTokenBalance.div(10 ** sell.decimals)
