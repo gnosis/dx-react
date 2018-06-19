d7731a8b2e9db06d4ecc3f98802159d299392957
Velenir <velenir@gmail.com>
2018-06-19 15:26:07 +0300

    use getSellTokenBalance selector in AuctionSellingGetting container

--- src/containers/AuctionSellingGetting/index.ts
+++ src/containers/AuctionSellingGetting/index.ts
@@ -1,13 +1,13 @@
 import { connect } from 'react-redux'
 import { setSellTokenAmount } from 'actions'
+import { getSellTokenBalance } from 'selectors'
 
 import { State } from 'types'
 import AuctionSellingGetting, { AuctionSellingGettingProps } from 'components/AuctionSellingGetting'
 
 const mapState = (state: State) => {
   // TODO: always have some price for every pair in RatioPairs
-  const { sell, buy, lastPrice: price } = state.tokenPair
-  const { [sell.address]: sellTokenBalance } = state.tokenBalances
+  const sellTokenBalance = getSellTokenBalance(state)
   const { sellAmount } = state.tokenPair
   const maxSellAmount = sellTokenBalance.div(10 ** sell.decimals)
 
