c46c7681726d894bdffc45e1b18e319066dcb4d9
Velenir <velenir@gmail.com>
2018-06-19 15:36:36 +0300

    remove console.logs

--- src/containers/OrderPanel/index.ts
+++ src/containers/OrderPanel/index.ts
@@ -9,12 +9,9 @@ const isTokenApproved = ({ approvedTokens, tokenPair: { sell = EMPTY_TOKEN, buy 
 
 const mapStateToProps = (state: State) => {
   const { tokenPair: { sell = EMPTY_TOKEN, buy = EMPTY_TOKEN, sellAmount } } = state
-  console.log('sell: ', sell);
   const sellTokenBalance = getSellTokenBalance(state)
-  console.log('sellTokenBalance: ', sellTokenBalance);
   // const { sellAmount } = state.tokenPair
   const maxSellAmount: BigNumber = sellTokenBalance.div(10 ** sell.decimals)
-  console.log('maxSellAmount: ', maxSellAmount);
 
   return {
     sellTokenSymbol: sell.symbol || sell.name || sell.address,
