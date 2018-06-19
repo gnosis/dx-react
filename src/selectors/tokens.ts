138f8f19d82da5931a4bda92bdf2a0def82f22dc
Velenir <velenir@gmail.com>
2018-06-19 15:36:58 +0300

    default to EMPTY_TOKEN in selectors/tokens

--- src/selectors/tokens.ts
+++ src/selectors/tokens.ts
@@ -1,18 +1,19 @@
 import { DefaultTokenObject, State } from 'types'
 import { ETH_ADDRESS } from 'globals'
 import { toBigNumber } from 'web3/lib/utils/utils.js'
+import { EMPTY_TOKEN } from 'globals'
 
 export const getTokenName = ({ symbol, name, address, isETH }: DefaultTokenObject) => {
   if (address === ETH_ADDRESS && isETH) return 'WETH'
   return symbol && symbol.toUpperCase() || name && name.toUpperCase() || address
 }
 
-export const getSellTokenBalance = ({ tokenPair: { sell }, tokenBalances }: State) => {
+export const getSellTokenBalance = ({ tokenPair: { sell = EMPTY_TOKEN }, tokenBalances }: State) => {
   const { [sell.address]: sellTokenBalance } = tokenBalances
   return sellTokenBalance === undefined ? toBigNumber(0) : sellTokenBalance
 }
 
-export const getBuyTokenBalance = ({ tokenPair: { buy }, tokenBalances }: State) => {
+export const getBuyTokenBalance = ({ tokenPair: { buy = EMPTY_TOKEN }, tokenBalances }: State) => {
   const { [buy.address]: buyTokenBalance } = tokenBalances
   return buyTokenBalance === undefined ? toBigNumber(0) : buyTokenBalance
 }
