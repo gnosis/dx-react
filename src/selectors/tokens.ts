9856a3de2f10efc2f4a48ba0f2897a82dc121df7
Velenir <velenir@gmail.com>
2018-06-19 15:22:53 +0300

    default to BN(0) for not yet fetched balance and abstract it to selectors for ease of use

--- src/selectors/tokens.ts
+++ src/selectors/tokens.ts
@@ -1,7 +1,18 @@
-import { DefaultTokenObject } from 'types'
+import { DefaultTokenObject, State } from 'types'
 import { ETH_ADDRESS } from 'globals'
+import { toBigNumber } from 'web3/lib/utils/utils.js'
 
 export const getTokenName = ({ symbol, name, address, isETH }: DefaultTokenObject) => {
   if (address === ETH_ADDRESS && isETH) return 'WETH'
   return symbol && symbol.toUpperCase() || name && name.toUpperCase() || address
+}
+
+export const getSellTokenBalance = ({ tokenPair: { sell }, tokenBalances }: State) => {
+  const { [sell.address]: sellTokenBalance } = tokenBalances
+  return sellTokenBalance === undefined ? toBigNumber(0) : sellTokenBalance
+}
+
+export const getBuyTokenBalance = ({ tokenPair: { buy }, tokenBalances }: State) => {
+  const { [buy.address]: buyTokenBalance } = tokenBalances
+  return buyTokenBalance === undefined ? toBigNumber(0) : buyTokenBalance
 }
