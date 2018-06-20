71af6f9cc3defd43b30221a93450ed1cc9f32377
Velenir <velenir@gmail.com>
2018-06-20 15:17:43 +0300

    wrap OrderPanel in RedirectHome

--- src/containers/OrderPanel/index.ts
+++ src/containers/OrderPanel/index.ts
@@ -3,12 +3,13 @@ import OrderPanel from 'components/OrderPanel'
 import { State, BigNumber } from 'types'
 import { getSellTokenBalance } from 'selectors'
 import { EMPTY_TOKEN } from 'globals';
+import { RedirectHomeIfNoAccountHOC } from 'components/RedirectIf';
 
 const isTokenApproved = ({ approvedTokens, tokenPair: { sell = EMPTY_TOKEN, buy = EMPTY_TOKEN } }: State) =>
   approvedTokens.has(sell.address) && approvedTokens.has(buy.address)
 
 const mapStateToProps = (state: State) => {
-  const { tokenPair: { sell = EMPTY_TOKEN, buy = EMPTY_TOKEN, sellAmount } } = state
+  const { tokenPair: { sell = EMPTY_TOKEN, buy = EMPTY_TOKEN, sellAmount }, blockchain } = state
   const sellTokenBalance = getSellTokenBalance(state)
   // const { sellAmount } = state.tokenPair
   const maxSellAmount: BigNumber = sellTokenBalance.div(10 ** sell.decimals)
@@ -20,8 +21,9 @@ const mapStateToProps = (state: State) => {
     buyTokenSymbol: buy.symbol || buy.name || buy.address,
     validSellAmount: validTokens && +sellAmount > 0 && maxSellAmount.greaterThanOrEqualTo(sellAmount),
     generatesMGN: isTokenApproved(state),
+    currentAccount: blockchain.currentAccount,
   }
 }
 
 
-export default connect(mapStateToProps)(OrderPanel)
+export default connect(mapStateToProps)(RedirectHomeIfNoAccountHOC(OrderPanel))
