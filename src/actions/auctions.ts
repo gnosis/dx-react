07d47159ea467363557ee49e0b9620adfd67e29a
Velenir <velenir@gmail.com>
2018-06-21 14:58:01 +0300

    added setAvailableAuctions auction

--- src/actions/auctions.ts
+++ src/actions/auctions.ts
@@ -1,3 +1,5 @@
 import { createAction } from 'redux-actions'
+import { AuctionsState } from 'types';
 
-export const setOngoingAuctions = createAction<any>('SET_ONGOING_AUCTIONS')
+export const setOngoingAuctions = createAction<Partial<AuctionsState>>('SET_ONGOING_AUCTIONS')
+export const setAvailableAuctions = createAction<string[]>('SET_AVAILABLE_AUCTIONS')
