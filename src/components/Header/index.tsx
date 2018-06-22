c285512f3bee15d190df8f055aedd0f964c1820b
W3stside <david.sato64@gmail.com>
2018-06-22 16:30:11 +0200

    pass prop to Header to render diff css for ContentPages
    1. passed in router.tsx withHeader

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,8 +6,12 @@ import Hamburger from 'components/Hamburger'
 import MenuFeeBalance from 'containers/MenuFeeBalance'
 import { Link } from 'react-router-dom'
 
-export const Header: React.SFC = () => (
-  <header>
+interface HeaderProps {
+  content?: boolean;
+}
+
+export const Header = ({ content }: HeaderProps) => (
+  <header className={content && 'solid-background'}>
     <div>
       <Link to="/" title="DutchX - Dutch Auction Exchange" className="logo"></Link>
       <MenuWallet />
