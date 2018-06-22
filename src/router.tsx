6060a1f9aa254b3eefab7290f12cea5841a7ffbf
W3stside <david.sato64@gmail.com>
2018-06-22 17:13:16 +0200

    added footer to Router under withHeaderAndFooter

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,7 @@ import { History } from 'history'
 import { hot } from 'react-hot-loader'
 
 import Header from 'components/Header'
+import Footer from 'components/Footer'
 import Home from 'containers/Home'
 import PageNotFound from 'components/PageNotFound'
 import Disclaimer from 'containers/Disclaimer'
@@ -22,19 +23,20 @@ interface AppRouterProps {
 }
 
 // TODO: consider redirecting from inside /order, /wallet, /auction/:nonexistent_addr to root
-const withHeader = (Component: React.ComponentClass | React.SFC, content?: boolean) => (props: any) => (
+const withHeaderAndFooter = (Component: React.ComponentClass | React.SFC, content?: boolean) => (props: any) => (
   <Fragment>
     <Header content={content}/>
     <Component {...props}/>
+    <Footer />
   </Fragment>
 )
 
-const HomeWH = withHeader(Home)
-const OrderPanelWH = withHeader(OrderPanel)
-const WalletPanelWH = withHeader(WalletPanel)
-const AuctionPanelWH = withHeader(AuctionPanel)
+const HomeWH = withHeaderAndFooter(Home)
+const OrderPanelWH = withHeaderAndFooter(OrderPanel)
+const WalletPanelWH = withHeaderAndFooter(WalletPanel)
+const AuctionPanelWH = withHeaderAndFooter(AuctionPanel)
 // true passed in to show different, solidBackgorund Header
-const ContentPageContainerWH = withHeader(ContentPageContainer, true)
+const ContentPageContainerWH = withHeaderAndFooter(ContentPageContainer, true)
 
 const AppRouter: React.SFC<AppRouterProps> = ({ history, disabled }) => {
   if (disabled) {
