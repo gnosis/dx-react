d4a03c6998962dd5fbb0996ee2b36f20309e381b
David Sato <david.sato64@gmail.com>
2018-06-25 00:32:29 +0200

    hotfixes for contract imports and new IPFS hash file

--- src/api/apiTesting.ts
+++ src/api/apiTesting.ts
@@ -13,6 +13,51 @@ export default async () => {
   } = await promisedContractsMap
 
   // TODO: hard-code actual addresses here
+  if (process.env.NODE_ENV !== 'production') {
+    return {
+      elements: [
+        {
+          name: 'ETHER',
+          symbol: 'ETH',
+          address: ETH_ADDRESS,
+          isETH: true,
+          decimals: 18,
+        },
+        {
+          name: 'WRAPPED ETHER',
+          symbol: 'WETH',
+          address: TokenETH.address,
+          decimals: (await TokenETH.decimals()).toNumber(),
+        },
+        {
+          name: 'GNOSIS TOKEN',
+          symbol: 'GNO',
+          address: TokenGNO.address,
+          decimals: (await TokenGNO.decimals()).toNumber(),
+        },
+        {
+          name: 'OMISEGO',
+          symbol: 'OMG',
+          address: TokenOMG.address,
+          decimals: TokenOMG.decimals ? (await TokenOMG.decimals()).toNumber() : 12,
+        },
+        {
+          name: 'RAIDEN',
+          symbol: 'RDN',
+          address: TokenRDN.address,
+          decimals: TokenRDN.decimals ? (await TokenRDN.decimals()).toNumber() : 6,
+        },
+        {
+          name: 'MAGNOLIA',
+          symbol: 'MGN',
+          address: TokenMGN.address,
+          decimals: 18,
+        },
+      ],
+      page: 1,
+      hasMorePages: false,
+    } as DefaultTokens
+  }
   return {
     elements: [
       {
@@ -33,18 +78,6 @@ export default async () => {
         symbol: 'GNO',
         address: TokenGNO.address,
         decimals: (await TokenGNO.decimals()).toNumber(),
-      },
-      {
-        name: 'OMISEGO',
-        symbol: 'OMG',
-        address: TokenOMG.address,
-        decimals: TokenOMG.decimals ? (await TokenOMG.decimals()).toNumber() : 12,
-      },
-      {
-        name: 'RAIDEN',
-        symbol: 'RDN',
-        address: TokenRDN.address,
-        decimals: TokenRDN.decimals ? (await TokenRDN.decimals()).toNumber() : 6,
       },
       {
         name: 'MAGNOLIA',
