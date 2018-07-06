6f01a297951aa1838b894fa519a9fb6c804a5ed8
Dmitry <Velenir@users.noreply.github.com>
2018-07-06 16:41:06 +0300

    Auction status revamped (#295)
    * different states for AuctionPanel children
    
    * fix types
    
    * Feature/auction status revamped david edits (#296)
    
    * render null if status === ended && claimed + lint
    
    * added conditional h3 tag w/Claiming message over button message
    
    * CSS edits to make disabled claim button functionality clearer
    
    * redirect uncaught errors to HomePage
    
    * transition auctionFooter height when nothing to claim and done
    
    * changed toFixed(4) to toFixed()

--- src/components/AuctionFooter/index.tsx
+++ src/components/AuctionFooter/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { BigNumber } from 'types'
+import { AuctionStatus as Status } from 'globals'
+import { Link } from 'react-router-dom'
 
 export interface AuctionFooterProps {
   sellTokenSymbol: string,
@@ -8,28 +10,54 @@ export interface AuctionFooterProps {
   buyAmount: BigNumber,
   sellDecimal: number,
   buyDecimal: number,
-  auctionEnded?: boolean
+  auctionEnded?: boolean,
+  status: Status,
 }
 
 const AuctionFooter: React.SFC<AuctionFooterProps> = ({
-  auctionEnded,
+  // auctionEnded,
   sellTokenSymbol,
-  buyTokenSymbol,
+  // buyTokenSymbol,
   sellAmount,
-  buyAmount,
+  // buyAmount,
   sellDecimal,
-  buyDecimal,
-}) => (
-  <div className="auctionFooter">
-    <span>
-      <small>AMOUNT SELLING</small>
-      <big>{sellAmount.div(10 ** sellDecimal).toString()} {sellTokenSymbol}</big>
-    </span>
-    {buyAmount.gt(0) && <span>
-      <small>{!auctionEnded && 'ESTIMATED'} GETTING TOTAL</small>
-      <big>{buyAmount.div(10 ** buyDecimal).toString()} {buyTokenSymbol}</big>
-    </span>}
-  </div>
-)
+  // buyDecimal,
+  status,
+}) => {
+  if (status === Status.PLANNED || status === Status.INIT || status === Status.ACTIVE) {
+    if (sellAmount.gt(0)) { return (
+      <div className="auctionFooter">
+        <span>
+          <small>AMOUNT DEPOSITED</small>
+          <big>{sellAmount.div(10 ** sellDecimal).toString()} {sellTokenSymbol}</big>
+        </span>
+      </div>
+    )
+    }
+
+    return (
+      <div className="auctionFooter">
+        <span>
+          <small>
+            {status === Status.ACTIVE ? 'You are not taking part in this auction' : 'You have no deposit in this auction'}
+          </small>
+          <big><Link to="/">Take part in the {status === Status.ACTIVE ? 'next' : ''} auction</Link></big>
+        </span>
+      </div>
+    )
+  }
+
+  if (status === Status.ENDED && sellAmount.gt(0)) { return (
+    <div className="auctionFooter">
+      <span>
+        <small>AMOUNT SOLD</small>
+        <big>{sellAmount.div(10 ** sellDecimal).toString()} {sellTokenSymbol}</big>
+      </span>
+    </div>
+  )
+  }
+
+  return <div className="auctionFooter closedFooter"></div>
+}
 
 export default AuctionFooter
