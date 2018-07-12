dbcaf9d0864c8af7ef7f79cba642c6b8772ebc48
Angel Rodriguez <anxolin@gmail.com>
2018-07-12 11:30:27 +0200

    Add the IPFS json token list file used in rinkeby

---
+++ test/resources/token-pairs-ipfs-rinkeby.js
@@ -0,0 +1,39 @@
+// npm run add2ipfs -- test/resources/token-pairs-ipfs-rinkeby.js
+
+module.exports = {
+  elements: [
+    {
+      symbol: 'ETH',
+      name: 'Ether',
+      address: '0x0',
+      decimals: 18,
+      isETH: true,
+    },
+    {
+      symbol: 'WETH',
+      name: 'Wrapped Ether',
+      address: '0xc778417e063141139fce010982780140aa0cd5ab',
+      decimals: 18,
+    },
+    {
+      symbol: 'RDN',
+      name: 'Raiden',
+      address: '0x3615757011112560521536258c1e7325ae3b48ae',
+      decimals: 18,
+    },
+    {
+      symbol: 'OMG',
+      name: 'OmiseGO',
+      address: '0x00df91984582e6e96288307e9c2f20b38c8fece9',
+      decimals: 18,
+    },
+  ],
+  pagination: {
+    endingBefore: null,
+    startingAfter: null,
+    limit: 20,
+    order: [{ param: 'symbol', direction: 'ASC' }],
+    previousUri: null,
+    nextUri: null,
+  },
+}
