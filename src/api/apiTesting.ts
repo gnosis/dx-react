7eeaa7c7cd9f3369e76321458e95bd8ca7a9c97a
David Sato <david.sato64@gmail.com>
2018-07-12 19:42:14 +0200

    removed omg and rdn from local tokens + moved event in initialize
    1. watch() event listener is in events.ts now

--- src/api/apiTesting.ts
+++ src/api/apiTesting.ts
@@ -7,57 +7,12 @@ export default async () => {
   const {
     TokenETH,
     TokenGNO,
-    TokenOMG,
-    TokenRDN,
     TokenMGN,
+    // TokenOMG,
+    // TokenRDN,
   } = await promisedContractsMap
 
   // TODO: hard-code actual addresses here
-  if (process.env.NODE_ENV === 'development') {
-    return {
-      elements: [
-        {
-          name: 'ETHER',
-          symbol: 'ETH',
-          address: ETH_ADDRESS,
-          isETH: true,
-          decimals: 18,
-        },
-        {
-          name: 'WRAPPED ETHER',
-          symbol: 'WETH',
-          address: TokenETH.address,
-          decimals: (await TokenETH.decimals()).toNumber(),
-        },
-        {
-          name: 'GNOSIS TOKEN',
-          symbol: 'GNO',
-          address: TokenGNO.address,
-          decimals: (await TokenGNO.decimals()).toNumber(),
-        },
-        {
-          name: 'OMISEGO',
-          symbol: 'OMG',
-          address: TokenOMG.address,
-          decimals: TokenOMG.decimals ? (await TokenOMG.decimals()).toNumber() : 12,
-        },
-        {
-          name: 'RAIDEN',
-          symbol: 'RDN',
-          address: TokenRDN.address,
-          decimals: TokenRDN.decimals ? (await TokenRDN.decimals()).toNumber() : 6,
-        },
-        {
-          name: 'MAGNOLIA',
-          symbol: 'MGN',
-          address: TokenMGN.address,
-          decimals: 18,
-        },
-      ],
-      page: 1,
-      hasMorePages: false,
-    } as DefaultTokens
-  }
   return {
     elements: [
       {
@@ -85,6 +40,18 @@ export default async () => {
         address: TokenMGN.address,
         decimals: 18,
       },
+      // {
+      //   name: 'OMISEGO',
+      //   symbol: 'OMG',
+      //   address: TokenOMG.address,
+      //   decimals: TokenOMG.decimals ? (await TokenOMG.decimals()).toNumber() : 12,
+      // },
+      // {
+      //   name: 'RAIDEN',
+      //   symbol: 'RDN',
+      //   address: TokenRDN.address,
+      //   decimals: TokenRDN.decimals ? (await TokenRDN.decimals()).toNumber() : 6,
+      // },
     ],
     page: 1,
     hasMorePages: false,
--- src/integrations/initialize.ts
+++ src/integrations/initialize.ts
@@ -8,9 +8,7 @@ import MetamaskProvider from './metamask'
 import ParityProvider from './parity'
 import RemoteProvider from './remote'
 
-import { watch, getBlock } from './filterChain'
-
-export const WATCHER_INTERVAL = 6000
+export const WATCHER_INTERVAL = 5000
 
 const networkById = {
   1: ETHEREUM_NETWORKS.MAIN,
@@ -76,7 +74,7 @@ export default async ({ registerProvider, updateProvider, updateMainAppState, re
         available = provider.walletAvailable,
         unlocked = !!(available && account),
         newState = { account, network, balance, available, unlocked, timestamp }
-        
+
         // if data changed
       if (shallowDifferent(provider.state, newState)) {
         console.log('app state is different')
@@ -114,12 +112,5 @@ export default async ({ registerProvider, updateProvider, updateMainAppState, re
     watcher(provider)
     // regularly refetch state
     setInterval(() => watcher(provider), WATCHER_INTERVAL)
-  })
-
-  // TEMP SAMPLE USAGE
-  watch(async (e, bl) => {
-    if (e) return console.error('Chain watching Error', e)
-    console.log('LATEST BLOCK')
-    console.log(await getBlock(bl))
   })
 }
