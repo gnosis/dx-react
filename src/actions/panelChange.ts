46eaae0d7ee969724e2e69879fa7107b891e80fd
David <david.sato64@gmail.com>
2018-07-26 21:18:34 +0200

    Fix/dx 455 dx 457 dx 458 dx 461 bug fixes (#312)
    * Added dutchx favicon
    
    * Fixed error displaying - fixed bug with provider being exited during tx
    
    * extrapolated pushToElement from contentPages into thunk
    
    * passed redux pushAndMoveToElement to ContentPage container
    
    * fixed contentPages components to move to top of pages
    
    * linked to mgn article in 'This pair will not generate MGN'
    
    * fixed MenuAuctions tokenPair link display

--- src/actions/panelChange.ts
+++ src/actions/panelChange.ts
@@ -1,4 +1,22 @@
 import { createAction } from 'redux-actions'
+import { Dispatch } from 'react-redux'
+import { push } from 'connected-react-router'
+import { grabElementID } from 'containers/ContentPages'
 
 // TODO: fill payload
 export const continueToOrder = createAction<any>('CONTINUE_TO_ORDER', () => { })
+
+export const pushAndMoveToElement = (sectionID: string, outsidePage?: string) => async (dispatch: Dispatch<any>) => {
+  if (outsidePage) {
+      await dispatch(push(outsidePage))
+    }
+
+  if (!sectionID) return
+
+  if (Array.from(grabElementID(sectionID).classList).some((className: string) => className === 'active')) {
+      return grabElementID(sectionID).scrollIntoView()
+    }
+
+  grabElementID(sectionID).classList.toggle('active')
+  return grabElementID(sectionID).scrollIntoView()
+}
