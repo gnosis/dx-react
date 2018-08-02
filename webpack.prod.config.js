dcd06a0f6d3ff0731c40b26507a9b9beef897524
Velenir <velenir@gmail.com>
2018-08-02 13:27:41 +0300

    use relative publicPath

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -20,7 +20,7 @@ module.exports = {
   context: path.join(__dirname, 'src'),
   entry: 'index.tsx',
   output: {
-    publicPath: '/',
+    publicPath: '',
     path: `${__dirname}/dist`,
     chunkFilename: '[name].[chunkhash].js',
     filename: '[name].[chunkhash].js',
