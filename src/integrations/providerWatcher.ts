01dbceec53666b7e1c27c5b07be1404ce3ef0495
David <david.sato64@gmail.com>
2018-08-10 11:36:27 +0200

    changed updateProvider getting incorrect param

--- src/integrations/providerWatcher.ts
+++ src/integrations/providerWatcher.ts
@@ -61,24 +61,24 @@ const providerInitAndWatcher = async (provider: WalletProvider, { updateMainAppS
         // check if initial load or wallet locked
 
       if (!unlocked) {
-          watcherLogger({
+        watcherLogger({
             logType: 'warn',
             status: 'WALLET LOCKED',
             info: 'Please unlock your wallet provider',
             updateState: false,
           })
           // if wallet locked, throw
-          throw 'Wallet locked'
-        }
+        throw 'Wallet locked'
+      }
       else {
-          watcherLogger({
+        watcherLogger({
             logType: 'warn',
             status: 'CONNECTED + WALLET UNLOCKED',
             info: 'Web3 provider connected + wallet unlocked',
             updateState: true,
           })
-          await updateMainAppState()
-        }
+        await updateMainAppState()
+      }
     }
   } catch (err) {
     console.warn(err)
@@ -91,7 +91,7 @@ const providerInitAndWatcher = async (provider: WalletProvider, { updateMainAppS
         // disable internal provider
       provider.state.unlocked = false
         // and dispatch action with { available: false }
-      updateProvider({ provider })
+      updateProvider(provider)
     }
     throw err
   }
