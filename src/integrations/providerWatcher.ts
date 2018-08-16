62bf2f5914449cd6ca6a65024d2e07d84c4f053c
David <david.sato64@gmail.com>
2018-08-16 14:03:06 +0200

    providerWatcher logic update to work with providerType + bug fix

--- src/integrations/providerWatcher.ts
+++ src/integrations/providerWatcher.ts
@@ -1,8 +1,7 @@
 import { WalletProvider } from 'integrations/types'
-import { promisify, shallowDifferent } from 'utils'
-import { Account, Balance } from 'types'
+import { shallowDifferent } from 'utils'
 import { getTime } from 'api'
-import { ETHEREUM_NETWORKS, networkById } from 'globals'
+import { getAccount, getNetwork, getBalance } from './provider'
 
 export const watcherLogger = ({ logType = 'log', status, info, updateState }: { logType: string, status: string, info: string, updateState: boolean }) =>
   console[logType](`
@@ -15,30 +14,12 @@ let prevTime: number
 
 // Fired on setInterval every 10 seconds
 const providerInitAndWatcher = async (provider: WalletProvider, { updateMainAppState, updateProvider, resetMainAppState }: any) => {
-
-  const getAccount = async (provider: WalletProvider): Promise<Account> => {
-    const [account] = await promisify(provider.web3.eth.getAccounts, provider.web3.eth)()
-
-    return account
-  }
-
-  const getNetwork = async (provider: WalletProvider): Promise<ETHEREUM_NETWORKS> => {
-    const networkId = await promisify(provider.web3.version.getNetwork, provider.web3.version)()
-
-    return networkById[networkId] || ETHEREUM_NETWORKS.UNKNOWN
-  }
-
-  const getBalance = async (provider: WalletProvider, account: Account): Promise<Balance> => {
-    const balance = await promisify(provider.web3.eth.getBalance, provider.web3.eth)(account)
-
-    return provider.web3.fromWei(balance, 'ether').toString()
-  }
-    // set block timestamp to provider state and compare
+  // set block timestamp to provider state and compare
   try {
     if (!provider.checkAvailability() || (window.navigator && !window.navigator.onLine)) throw new Error('Provider and/or internet issues')
     provider.state.timestamp = prevTime
 
-    const [account, network, timestamp] = await Promise.all<Account, ETHEREUM_NETWORKS, number>([
+    const [account, network, timestamp] = await Promise.all([
         getAccount(provider),
         getNetwork(provider),
         getTime(),
@@ -51,13 +32,13 @@ const providerInitAndWatcher = async (provider: WalletProvider, { updateMainAppS
       // if data changed
     if (shallowDifferent(provider.state, newState)) {
       console.log('app state is different')
-      console.log('was: ', newState)
-      console.log('now: ', provider.state)
+      console.log('was: ', provider.state)
+      console.log('now: ', newState)
 
         // reset module timestamp with updated timestamp
       prevTime = timestamp
         // dispatch action with updated provider state
-      updateProvider({ provider: provider.providerName, ...newState })
+      updateProvider({ provider: provider.providerType, ...newState })
         // check if initial load or wallet locked
 
       if (!unlocked) {
@@ -90,7 +71,7 @@ const providerInitAndWatcher = async (provider: WalletProvider, { updateMainAppS
         // disable internal provider
       provider.state.unlocked = false
         // and dispatch action with { available: false }
-      updateProvider(provider)
+      updateProvider({ provider: provider.providerType })
     }
     throw err
   }
