84b4c71b4e5740033fb68df8497d56cfc71af0fb
David <david.sato64@gmail.com>
2018-08-28 18:56:58 +0200

    changed netBlock logic as requirements changed

--- src/block.ts
+++ src/block.ts
@@ -4,44 +4,38 @@ export const geoBlockedCountryCodes = new Set(blocked_codes)
 
 export const isGeoBlocked = async () => {
   try {
-      const res = await fetch('https://geoip.gnosis.pm/json/')
+    const res = await fetch('https://geoip.gnosis.pm/json/')
 
       // this DOES NOT block even if the URL above starts returning 404
-      if (!res.ok) return false
+    if (!res.ok) return false
 
-      const { country_code } = await res.json()
+    const { country_code } = await res.json()
 
-      return geoBlockedCountryCodes.has(country_code)
-    } catch (error) {
-      console.error('Geo Blocking check is unavailable:', error.message || error, 'This is most likely a client side network connectivity issue - please retry connecting to the internet and refreshing the page.')
+    return geoBlockedCountryCodes.has(country_code)
+  } catch (error) {
+    console.error('Geo Blocking check is unavailable:', error.message || error, 'This is most likely a client side network connectivity issue - please retry connecting to the internet and refreshing the page.')
 
       // this does NOT block if there is a network error, e.g. URL is blocked
-      return false
-    }
+    return false
+  }
 }
 
-export const isNetBlocked = async () => {
-  if (typeof window === 'undefined') return false
-  const { hostname } = window.location
-    // allow anything when run locally
-  if (hostname === 'localhost' || hostname === '0.0.0.0') return false
-
-    // no walletextension detected, different error - download wallet error
-  if (!window.web3) return false
+export const isNetBlocked = async (idToAllow: string | number) => {
+  // no walletextension detected, different error - download wallet error
+  if (typeof window === 'undefined' || !window.web3) return false
 
   try {
-      const id = await new Promise((res, rej) => {
+    const id = await new Promise((res, rej) => {
         window.web3.version.getNetwork((e: Error, r: string) => e ? rej(e) : res(r))
       })
       // allow Rinkeby and local testrpc (id = Date.now())
       //                      Apr 29 2018
-      if (id === '4' || id > 1525000000000) return false
-    } catch (error) {
-      console.error(error)
+    if (id === idToAllow || id > 1525000000000) return false
+  } catch (error) {
+    console.error(error)
       // web3 didn't get network, disconnected?
-      return false
-    }
+    return false
+  }
 
   return true
 }
-
