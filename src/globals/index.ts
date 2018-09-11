3763d537fe6700445106f39860cd56d8c954618e
David <david.sato64@gmail.com>
2018-09-11 11:44:09 +0200

    commented in current token lists - error fix from ETHBerlin commit

--- src/globals/index.ts
+++ src/globals/index.ts
@@ -85,10 +85,17 @@ export const EMPTY_TOKEN: DefaultTokenObject = {
 // Network token list hashes (latest versions)
 export const TESTING_TOKEN_LIST_HASH = 'QmXgUiWTumXghNuLk3vAypVeL4ycVkNKhrtWfvFHoQTJAM'
 
-export const RINKEBY_TOKEN_LIST_HASH = process.env.NODE_ENV === 'production' ?
-  'QmW4NCDDZRexP5FVpMQXxNWwFHTQjCGeb5d8ywLs2XRJxR' : 'QmXVXUCSV6EEcXf2RKkv8QBLR12SiBgXVUDP1BLcTx8VFr'
+export const RINKEBY_TOKEN_LIST_HASH = process.env.NODE_ENV === 'production' ? 'QmW4NCDDZRexP5FVpMQXxNWwFHTQjCGeb5d8ywLs2XRJxR' : 'QmfB3fRGacBseNiBMhKFaYoEGDyiWnUCBPsE7Xo3sKqSyi'
   
- // 'QmfB3fRGacBseNiBMhKFaYoEGDyiWnUCBPsE7Xo3sKqSyi'
+
+// PoolX token list for PRODUCTION
+// 'QmXVXUCSV6EEcXf2RKkv8QBLR12SiBgXVUDP1BLcTx8VFr'
+
+// Development token list
+// 'QmfB3fRGacBseNiBMhKFaYoEGDyiWnUCBPsE7Xo3sKqSyi'
+
+// Production token list
+// 'QmW4NCDDZRexP5FVpMQXxNWwFHTQjCGeb5d8ywLs2XRJxR'
 
 export const KOVAN_TOKEN_LIST_HASH = 'QmVk68VH1D2uGx2LUGXsrfvKHQiA1R4sjw8cw4so33DPsw'
 export const MAINNET_TOKEN_LIST_HASH = 'QmZwJb4N9tSXme2bPoPtBg5Mz5pgct8oLVbTaqHBSsURSR'
