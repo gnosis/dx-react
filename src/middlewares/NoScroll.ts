fc370fcad9a94b33711846ffb8e5b587ea3793f0
David <david.sato64@gmail.com>
2018-09-21 23:56:15 +0200

    fix attempt at noScroll MW incorrectly showing scroll-y bar + lint

--- src/middlewares/NoScroll.ts
+++ src/middlewares/NoScroll.ts
@@ -1,17 +1,16 @@
-// document.body.classList.add("home");
-
 import { Middleware, Action } from 'redux'
 
 const NoScroll = () => (next: Function) => async (action: any) => {
   const { type } = action as Action
-
-  if (action.payload && action.payload.modalProps && action.payload.modalProps.txData || type !== 'CLOSE_MODAL' && type !== 'OPEN_MODAL') return next(action as Action)
+  if (action.payload && action.payload.modalProps && action.payload.modalProps.header !== 'Order confirmation' || type !== 'CLOSE_MODAL' && type !== 'OPEN_MODAL') return next(action as Action)
 
   try {
     if (type === 'OPEN_MODAL') {
       document && document.body && document.body.classList.add('noScroll')
+      document && document.getElementById('root') && document.getElementById('root').classList.add('noScroll')
     } else if (type === 'CLOSE_MODAL') {
       document && document.body && document.body.classList.remove('noScroll')
+      document && document.getElementById('root') && document.getElementById('root').classList.remove('noScroll')
     }
 
     return next(action as Action)
