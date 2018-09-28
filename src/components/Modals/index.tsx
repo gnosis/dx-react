9e41125377d689461667c8112f3bdfe490262554
Michel <michel@Michels-MacBook-Pro.local>
2018-09-28 17:24:23 +0200

    misc changes

--- src/components/Modals/index.tsx
+++ src/components/Modals/index.tsx
@@ -53,7 +53,6 @@ export const TransactionModal: React.SFC<TransactionModalProps> = ({
         {txData.useOWL && <span>Fees paid in OWL:<hr /><strong>{`${(((txData.sellAmount as BigNumber).minus(txData.sellAmountAfterFee)).div(2)).toFixed(FIXED_DECIMALS)}OWL (=${((txData.sellAmount as BigNumber).minus(txData.sellAmountAfterFee)).toFixed(FIXED_DECIMALS)} ${txData.tokenA.symbol || txData.tokenA.name})`}</strong></span>}
         <span>Fees paid in {`${txData.tokenA.symbol || txData.tokenA.name}`}:<hr /><strong>{`${txData.useOWL ? (((txData.sellAmount as BigNumber).minus(txData.sellAmountAfterFee)).div(2)).toFixed(FIXED_DECIMALS) : ((txData.sellAmount as BigNumber).minus(txData.sellAmountAfterFee)).toFixed(FIXED_DECIMALS)}${txData.tokenA.symbol || txData.tokenA.name}`}</strong></span>
         <span>Amount deposited into auction:<hr /><strong>{`${txData.sellAmountAfterFee.toFixed(FIXED_DECIMALS)}${txData.tokenA.symbol || txData.tokenA.name}`}</strong></span>
-        <span>Amount deposited:<hr /><strong>{`${txData.sellAmount} (without fees applied)`}</strong></span>
             {(txData.tokenA.symbol || txData.tokenA.name) &&
             <span>Token depositing:<hr /><strong>{`${txData.tokenA.symbol || txData.tokenA.name}${(txData.tokenA.name && txData.tokenA.symbol) && ' (' + txData.tokenA.name + ')'}`}</strong></span>
             }
