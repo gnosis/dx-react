8b503fb180cb619709d7495313f0b58e2f3f28ca
Velenir <velenir@gmail.com>
2018-10-19 17:08:41 +0300

    calc limit for usd

--- src/components/AuctionSellingGetting/index.tsx
+++ src/components/AuctionSellingGetting/index.tsx
@@ -1,22 +1,48 @@
 import React, { Component } from 'react'
 
 import { Balance, BigNumber } from 'types'
+import { getTokenPriceInUSD } from 'api'
 
 /* CONSIDER ADDING GAS_COST */
 export interface AuctionSellingGettingProps {
   maxSellAmount: BigNumber,
   buyTokenSymbol: string,
   sellTokenSymbol: string,
+  sellTokenAddress: string,
   sellAmount: Balance,
   buyAmount: Balance,
   setSellTokenAmount(props: any): any,
 }
 
-class AuctionSellingGetting extends Component<AuctionSellingGettingProps> {
+interface AuctionSellingGettingState {
+  sellTokenInUSD: BigNumber,
+}
+
+const MAX_SELL_USD = 500
+
+class AuctionSellingGetting extends Component<AuctionSellingGettingProps, AuctionSellingGettingState> {
+  componentDidMount() {
+    this.tokenInUSD()
+  }
+
+  componentDidUpdate(prevProps: AuctionSellingGettingProps) {
+    if (prevProps.sellTokenAddress !== this.props.sellTokenAddress) this.tokenInUSD()
+  }
+
+  tokenInUSD = async () => {
+    if (!this.props.sellTokenAddress) return
+    const tokenUSDPrice = await getTokenPriceInUSD(this.props.sellTokenAddress)
+    console.log('tokenUSDPrice: ', tokenUSDPrice.toString())
+    this.setState({
+      sellTokenInUSD: tokenUSDPrice,
+    })
+  }
+
   onChange = (e: React.ChangeEvent<HTMLInputElement & HTMLFormElement>) => {
     // TODO: consider using e.target.value.match(/^(0|[1-9]\d*)(\,|.\d+)?(e-?(0|[1-9]\d*))?$/i)
     const input = e.target
-    const { sellAmount, setSellTokenAmount, maxSellAmount } = this.props
+    const { sellAmount, setSellTokenAmount, maxSellAmount, sellTokenSymbol } = this.props
+    const { sellTokenInUSD } = this.state
 
     let { value } = input
     // if (value.length < 1) return setSellTokenAmount({ sellAmount: '0' })
@@ -28,12 +54,18 @@ class AuctionSellingGetting extends Component<AuctionSellingGettingProps> {
 
     const validValue = !!(+value)
     // validate maxSellAmount isnt exceeded but make sure value is castable to type number
-    if (validValue && sellAmount) {
+    console.log('validValue && sellAmount: ', validValue, sellAmount)
+    if (validValue && value) {
       if (maxSellAmount.lessThanOrEqualTo(value)) {
         input.setCustomValidity(`Amount available for sale is ${maxSellAmount.toString()}`)
         input.reportValidity()
+      } else if (sellTokenInUSD && sellTokenInUSD.mul(value).gt(MAX_SELL_USD)) {
+        input.setCustomValidity(`You are trading on Mainnet - for the moment, we limit your deposit to an equivalent of ${MAX_SELL_USD}USD (${sellTokenInUSD.mul(value).round(4).toString()}${sellTokenSymbol})`)
+        input.reportValidity()
       } else {
+        console.log('Reset validity', input.validationMessage)
         input.setCustomValidity('')
+        input.reportValidity()
       }
     } else {
       input.setCustomValidity('Please enter a valid number and/or valid separator')
