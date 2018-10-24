8254b73aeb189cb57f89d1cf2285403f934eec9c
David <david.sato64@gmail.com>
2018-10-24 17:31:34 +0200

    created AppLoadBypass Middleware duck
    1. reducer/blockchain
    2. passed into store etc
    3. allows users to bypass app loading shit

---
+++ src/middlewares/AppLoadBypass.ts
@@ -0,0 +1,42 @@
+// action.type: @@router/LOCATION_CHANGE
+// action.payload: location.pathname === '/cookies'
+
+import { Middleware, Action } from 'redux'
+import { createAction } from 'redux-actions'
+import { State } from 'types'
+
+export const setAppLoadBypass = createAction<boolean>('SET_APP_LOAD_BYPASS')
+
+const AppLoadBypass = ({ dispatch, getState }: { dispatch: Function, getState: () => State }) => (next: Function) => async (action: any) => {
+  const { payload, type } = action as { payload: any, type: string }
+  const { router: { location: { pathname } } }: any = getState()
+
+  if (type !== '@@router/LOCATION_CHANGE') return next(action as Action)
+
+  const pathMatch = (toCheck: string, match: boolean = true) =>
+        match
+            ?
+        (toCheck === '/cookies' || toCheck.includes('/content/'))
+            :
+        (toCheck !== '/cookies' && !toCheck.includes('/content/'))
+
+  try {
+      if (pathMatch(pathname)) {
+          console.debug('LOCATION MATCH')
+          if (
+                pathMatch(payload.location.pathname, false)
+            ) {
+              dispatch(setAppLoadBypass(true))
+            }
+        } else {
+          console.debug('LOCATION MIS-MATCH')
+          dispatch(setAppLoadBypass(false))
+        }
+
+      return next(action as Action)
+    } catch (e) {
+    // Unable to load or parse stored state, proceed as usual
+    }
+}
+
+export default AppLoadBypass as Middleware
