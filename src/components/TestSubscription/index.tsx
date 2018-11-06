c3bff52cc348e849cc120680c693be8d6af10394
David <david.sato64@gmail.com>
2018-11-06 15:19:52 +0100

    clean up

--- src/components/TestSubscription/index.tsx
+++ src/components/TestSubscription/index.tsx
@@ -1,34 +1,12 @@
 import React from 'react'
-// @ts-ignore
-
-import { createSubscription } from 'create-subscription'
 import VisualComp from './visual'
+// @ts-ignore
+import { createSubscription } from 'create-subscription'
 import { grabProviderState } from 'integrations/provider'
 
 const Web3 = require('web3')
 Web3.providers.HttpProvider.prototype.send = Web3.providers.HttpProvider.prototype.sendAsync
-const web3 = new Web3(window.web3.currentProvider);
-(window as any).wweebb3 = web3
-// const address = web3.eth.defaultAccount
-// let balance = web3.eth.getBalance(address)
-// console.log('TCL: balance', balance)
-
-// const filter = web3.eth.filter('latest')
-// filter.watch((err: any, res: any) => {
-//   if (err) {
-//     console.log(`Watch error: ${err}`)
-//   } else {
-//     // Update balance
-//     web3.eth.getBalance(address, (err: any, bal: any) => {
-//       if (err) {
-//         console.log(`getBalance error: ${err}`)
-//       } else {
-//         balance = bal
-//         console.log(`Balance [${address}]: ${web3.fromWei(balance, 'ether')}`)
-//       }
-//     })
-//   }
-// })
+const web3 = new Web3(window.web3.currentProvider)
 
 const fakeProvider = {
   name: 'FAKE PROVIDER',
@@ -46,16 +24,8 @@ const filterObject = {
   },
   subscribe(cb: any) {
     let prevAcc = web3.eth.accounts[0]
+    // initial state grab
     grabProviderState(fakeProvider as any).then(cb)
-    // web3.eth.getBalance(web3.eth.accounts[0], (err: any, balance: any) => {
-    //   if (err) {
-    //     console.log(`getBalance error: ${err}`)
-    //     cb({ error: err })
-    //   } else {
-    //     console.log(`Balance [${address}]: ${web3.fromWei(balance, 'ether')}`)
-    //     cb({ account: web3.eth.accounts[0], balance })
-    //   }
-    // })
 
     setInterval(async () => {
       if (web3.eth.accounts[0] !== prevAcc) {
@@ -63,18 +33,12 @@ const filterObject = {
         prevAcc = web3.eth.accounts[0]
         const accData = await grabProviderState(fakeProvider as any)
         cb(accData)
-        // web3.eth.getBalance(web3.eth.accounts[0], (err: any, balance: any) => {
-        //   if (err) {
-        //     console.log(`getBalance error: ${err}`)
-        //     cb({ error: err })
-        //   } else {
-        //     console.log(`Balance [${address}]: ${web3.fromWei(balance, 'ether')}`)
-        //     cb({ account: web3.eth.accounts[0], balance })
-        //   }
-        // })
       }
     }, 8000)
+
+    // create filter listening for latest new blocks
     filter = web3.eth.filter('latest')
+    // watch and update state on new block
     filter.watch(async (err: any, res: any) => {
       console.log('res: ', res)
       if (err) {
@@ -82,23 +46,10 @@ const filterObject = {
         cb({ error: err })
       } else {
         // Update balance
-        console.log('web3.eth.defaultAccount: ', web3.eth.accounts[0])
-        // web3.eth.accounts[0] && web3.eth.getBalance(web3.eth.accounts[0], (err: any, balance: any) => {
-        //   if (err) {
-        //     console.log(`getBalance error: ${err}`)
-        //     cb({ error: err })
-        //   } else {
-        //     console.log(`Balance [${address}]: ${web3.fromWei(balance, 'ether')}`)
-        //     cb({ account: web3.eth.accounts[0], balance, blockHash: res })
-        //   }
-        // })
-
-        // web3.eth.getBlock(res, )
         const accData = await grabProviderState(fakeProvider as any)
         cb(accData)
       }
     })
-    // console.log('await grabProviderState(fakeProvider as any): ', await grabProviderState(fakeProvider as any));
   },
   unsubscribe() {
     return filter.stopWatching()
@@ -123,11 +74,11 @@ const Subscription: any = createSubscription({
   },
 })
 
-const TestSubscription = () => (
+const ProviderSubscription = () => (
   < Subscription source={filterObject} >
     {(value: any) => <VisualComp {...value} />}
   </Subscription >
 
 )
 
-export default TestSubscription
+export default ProviderSubscription
