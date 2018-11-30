295247b50d0111b3863560c4c7a4d74f0dab60ea
David <david.sato64@gmail.com>
2018-11-30 16:07:34 +0100

    Creation of ClaimOnly component
    1. used in Home component with some logic restructuring

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,18 +3,24 @@ import React from 'react'
 import NoWallet from 'containers/NoWallet'
 import TextSquare from 'components/TextSquare'
 import TokenPicker from 'containers/TokenPicker'
+import { ClaimOnly } from 'components/NoWallet'
 
 export interface HomeProps {
   walletEnabled: boolean,
   showPicker?: boolean,
   needsTokens?: boolean,
+  claimOnly?: boolean,
 }
 
-const Home: React.SFC<HomeProps> = ({ walletEnabled, showPicker }) =>
+const Home: React.SFC<HomeProps> = ({ claimOnly, walletEnabled, showPicker }) =>
   <section className="home">
-    <TextSquare />
-    {(showPicker || walletEnabled) ?
-      <TokenPicker to="/order" showPair={showPicker} /> : <NoWallet />
+    <TextSquare claimOnly />
+    {
+      !walletEnabled ? <NoWallet />
+        :
+      claimOnly ? <ClaimOnly />
+        :
+      <TokenPicker to="/order" showPair={showPicker} />
     }
   </section>
 
