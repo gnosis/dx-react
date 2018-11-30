e6cd2799a15176382bd85ead020192ea6bb3eebb
David <david.sato64@gmail.com>
2018-11-30 16:08:48 +0100

    TextSquare: claimOnly flag to remove certain elements on claim_only

--- src/components/TextSquare/index.tsx
+++ src/components/TextSquare/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ButtonCTA from 'components/ButtonCTA'
 
-export const TextSquare: React.SFC = () =>
+interface TextSquareProps {
+  claimOnly?: boolean,
+}
+
+export const TextSquare: React.SFC<TextSquareProps> = ({ claimOnly }) =>
   <div className="intro">
     <h1>
       <div className="homeTitle">Trading Platform</div>
@@ -9,22 +13,26 @@ export const TextSquare: React.SFC = () =>
     </h1>
 
     <div className="textSquareInnerContainer">
-    This trading interface allows you to interact with
-    the DutchX decentralized trading protocol for ERC20 tokens,
-    determining a fair value for tokens based on the Dutch auction principle.
-    <br/><br/>
-    No account needed. Direct trades between peers through smart contracts.
-    <br/><br/>
-    <ol>
-      <li>Choose the tokens you would like to trade</li>
-      <li>Specify the amount to deposit</li>
-      <li>Submit the order via your wallet provider</li>
-    </ol>
-    <br/>
-    Your order is automatically deposited into the next running auction - no strategy needed!
-    <br/><br/>
+      This trading interface allows you to interact with
+      the DutchX decentralized trading protocol for ERC20 tokens,
+      determining a fair value for tokens based on the Dutch auction principle.
+      <br/><br/>
+      No account needed. Direct trades between peers through smart contracts.
+      <br/><br/>
+      {!claimOnly &&
+        <>
+          <ol>
+            <li>Choose the tokens you would like to trade</li>
+            <li>Specify the amount to deposit</li>
+            <li>Submit the order via your wallet provider</li>
+          </ol>
+          <br/>
+          Your order is automatically deposited into the next running auction - no strategy needed!
+          <br/><br/>
 
-      <ButtonCTA to="/content/HowItWorks" className={null}><strong>Slow &amp; Fair -</strong>How it works</ButtonCTA>
+          <ButtonCTA to="/content/HowItWorks" className={null}><strong>Slow &amp; Fair -</strong>How it works</ButtonCTA>
+        </>
+      }
     </div>
   </div>
 
