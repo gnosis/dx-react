6450fbbbec6c4cbc0614c50fe39ea0c5f5fcfad3
David <david.sato64@gmail.com>
2018-12-04 18:18:42 +0100

    styling

--- src/components/DXContractSwitcher/index.tsx
+++ src/components/DXContractSwitcher/index.tsx
@@ -5,82 +5,96 @@ import { getNetwork } from 'integrations/provider'
 interface DXContractPickerState {
   ALL_OLD_CONTRACT_ADDRESSES: {},
   CONTRACT_ADDRESSES_TO_USE: {
-      version: string,
-      networks: {
-          '1': string,
-          '4': string,
-          '42': string,
-        },
-    },
+    version: string,
+    networks: {
+        '1': string,
+        '4': string,
+        '42': string,
+      },
+  },
   DX_ADDRESS_TO_USE: string,
   VERSION_TO_USE: string,
   network: string,
 }
 
+const flexContainers = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexFlow: 'row wrap',
+
+  margin: '5px 0',
+}
+
 export default class DXContractPicker extends React.Component<{}, DXContractPickerState> {
   state = {
-      ALL_OLD_CONTRACT_ADDRESSES: undefined,
-      CONTRACT_ADDRESSES_TO_USE: undefined,
-      VERSION_TO_USE: undefined,
-      network: undefined,
-    } as DXContractPickerState
+    ALL_OLD_CONTRACT_ADDRESSES: undefined,
+    CONTRACT_ADDRESSES_TO_USE: undefined,
+    VERSION_TO_USE: undefined,
+    network: undefined,
+  } as DXContractPickerState
 
   async componentDidMount() {
-      const [ALL_OLD_CONTRACT_ADDRESSES, CONTRACT_ADDRESSES_TO_USE, network] = await Promise.all([
-          localForage.getItem<any>('ALL_OLD_CONTRACT_ADDRESSES'),
-          localForage.getItem<any>('CONTRACT_ADDRESSES_TO_USE'),
-          getNetwork(window as any, true),
-        ])
+    const [ALL_OLD_CONTRACT_ADDRESSES, CONTRACT_ADDRESSES_TO_USE, network] = await Promise.all([
+        localForage.getItem<any>('ALL_OLD_CONTRACT_ADDRESSES'),
+        localForage.getItem<any>('CONTRACT_ADDRESSES_TO_USE'),
+        getNetwork(window as any, true),
+      ])
 
-      return this.setState({
-          ALL_OLD_CONTRACT_ADDRESSES,
-          CONTRACT_ADDRESSES_TO_USE,
-          DX_ADDRESS_TO_USE: CONTRACT_ADDRESSES_TO_USE['DutchExchange'][network].address,
-          VERSION_TO_USE: CONTRACT_ADDRESSES_TO_USE['DutchExchange'].version,
-          network,
-        })
-    }
+    return this.setState({
+        ALL_OLD_CONTRACT_ADDRESSES,
+        CONTRACT_ADDRESSES_TO_USE,
+        DX_ADDRESS_TO_USE: CONTRACT_ADDRESSES_TO_USE['DutchExchangeProxy'][network].address,
+        VERSION_TO_USE: CONTRACT_ADDRESSES_TO_USE['DutchExchangeProxy'].version,
+        network,
+      })
+  }
 
   changeDXAddress = async (e: ChangeEvent<HTMLSelectElement>) => {
-      const { ALL_OLD_CONTRACT_ADDRESSES, network } = this.state
+    const { ALL_OLD_CONTRACT_ADDRESSES, network } = this.state
         // value here = network-list-version e.g 1.0.9 or 1.1.0
-      const { value: version } = e.target
+    const { value: version } = e.target
 
-      const OLD_CONTRACT_OBJECT_TO_USE = ALL_OLD_CONTRACT_ADDRESSES[version]
+    const OLD_CONTRACT_OBJECT_TO_USE = ALL_OLD_CONTRACT_ADDRESSES[version]
 
-      this.setState({
-          VERSION_TO_USE: version,
-          CONTRACT_ADDRESSES_TO_USE: OLD_CONTRACT_OBJECT_TO_USE,
-          DX_ADDRESS_TO_USE: OLD_CONTRACT_OBJECT_TO_USE['DutchExchange'][network].address,
-        })
-      await localForage.setItem('CONTRACT_ADDRESSES_TO_USE', OLD_CONTRACT_OBJECT_TO_USE)
+    this.setState({
+        VERSION_TO_USE: version,
+        CONTRACT_ADDRESSES_TO_USE: OLD_CONTRACT_OBJECT_TO_USE,
+        DX_ADDRESS_TO_USE: OLD_CONTRACT_OBJECT_TO_USE['DutchExchangeProxy'][network].address,
+      })
+    await localForage.setItem('CONTRACT_ADDRESSES_TO_USE', OLD_CONTRACT_OBJECT_TO_USE)
 
-      return location.reload()
-    }
+    return location.reload()
+  }
 
   render() {
-      const { ALL_OLD_CONTRACT_ADDRESSES, DX_ADDRESS_TO_USE, VERSION_TO_USE } = this.state
+    const { ALL_OLD_CONTRACT_ADDRESSES, DX_ADDRESS_TO_USE, VERSION_TO_USE } = this.state
 
-      if (!ALL_OLD_CONTRACT_ADDRESSES) return null
+    if (!ALL_OLD_CONTRACT_ADDRESSES) return null
 
-      return (
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexFlow: 'row wrap' }}>
-                <h3 style={{ margin: '0 5px' }}>DutchX Version: </h3>
-                <select
-                    onChange={this.changeDXAddress}
-                    title={DX_ADDRESS_TO_USE}
-                    value={VERSION_TO_USE}
-                >
-                    {Object.keys(ALL_OLD_CONTRACT_ADDRESSES).map((version: string, i: number) =>
-                        <option
-                            key={i + version}
-                            value={version}
-                        >
-                            {version}
-                        </option>,
-                    )}
-                </select>
+    return (
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexFlow: 'row wrap', background: '#fbf8e3' }}>
+                <div style={flexContainers}>
+                    <h3 style={{ margin: '0 5px' }}>DutchX Version: </h3>
+                    <select
+                        onChange={this.changeDXAddress}
+                        title={DX_ADDRESS_TO_USE}
+                        value={VERSION_TO_USE}
+                    >
+                        {Object.keys(ALL_OLD_CONTRACT_ADDRESSES).map((version: string, i: number) =>
+                            <option
+                                key={i + version}
+                                value={version}
+                            >
+                                {version}
+                            </option>,
+                        )}
+                    </select>
+                </div>
+                <div style={flexContainers}>
+                    <h3 style={{ margin: '0 5px' }}>DutchX Address: </h3><p>{DX_ADDRESS_TO_USE}</p>
+                </div>
             </div>
-        )
-    }
+      )
+  }
 }
