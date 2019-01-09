bff213b13c35089a40abd8ec5271fd0b7a1aae9b
David <david.sato64@gmail.com>
2019-01-09 14:28:52 +0100

    DX-671 // TOS vs Network Check (#445)
    * set TOS equal for all networks
    
    * updated acceptDisclaimer dispatcher
    
    * removed log + updated types for cookieSettings / settings
    
    * expanded asyncLoadSettings to check network/verification
    
    1. checks network and updates verification/disclaimer logic
    
    * changes as suggested by Dima
    
    1. shorten asyncSaveSettings logic

--- src/reducers/settings.ts
+++ src/reducers/settings.ts
@@ -1,13 +1,13 @@
 import { handleActions } from 'redux-actions'
 
 import { saveSettings } from 'actions/settings'
-import { Settings } from 'types'
+import { Settings, CookieSettings } from 'types'
 
-const reducer = handleActions<Settings>({
+const reducer = handleActions<Settings & CookieSettings>({
   [saveSettings.toString()]: (state, action) => ({
     ...state,
     ...action.payload,
   }),
-}, {} as Settings)
+}, {} as Settings & CookieSettings)
 
 export default reducer
