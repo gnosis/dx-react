807cdd0259a06bdc505246ced64048a856628dd6
David <david.sato64@gmail.com>
2019-01-17 11:31:46 +0100

    contract updates + truffle config changes

--- src/api/contracts.ts
+++ src/api/contracts.ts
@@ -236,8 +236,8 @@ async function init(provider: Provider) {
     delete deployedContracts.DutchExchangeProxy
     delete deployedContracts.TokenOWLProxy
 
-    if (process.env.FE_CONDITIONAL_ENV === 'development') {
-      console.log(deployedContracts)
+    if (process.env.FE_CONDITIONAL_ENV !== 'development') {
+      console.debug(deployedContracts)
     }
 
     console.warn(`
