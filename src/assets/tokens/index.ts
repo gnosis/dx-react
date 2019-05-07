0079e8fd2220798da348e2d8b6d9d1f6dce0fb36
David <david.sato64@gmail.com>
2019-05-07 16:31:24 -0700

    token svgs are NOT dynamicly shown
    1.
    a. tokens svgs are currently hardcoded into SCSS from data-coin
    b. and shown thru background prop
    c. changed some code to potentially allow future dynamic token add for svg

---
+++ src/assets/tokens/index.ts
@@ -0,0 +1,17 @@
+const allSVGs = {
+  DEFAULT_TOKEN: require('./DEFAULT_TOKEN.svg'),
+  ETH: require('./ETH.svg'),
+  WETH: require('./WETH.svg'),
+
+  DAI: require('./DAI.svg'),
+  GEN: require('./GEN.svg'),
+  GNO: require('./GNO.svg'),
+  GNT: require('./GNT.svg'),
+  KNC: require('./KNC.svg'),
+  MKR: require('./MKR.svg'),
+  OMG: require('./OMG.svg'),
+  RDN: require('./RDN.svg'),
+  REP: require('./REP.svg'),
+}
+
+export default allSVGs
