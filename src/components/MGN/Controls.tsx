0905725e8a4dfb91569a9e371de5d706d2d74a4f
Velenir <velenir@gmail.com>
2019-06-11 12:03:46 +0300

    space out  p.control-info text

--- src/components/MGN/Controls.tsx
+++ src/components/MGN/Controls.tsx
@@ -39,66 +39,70 @@ const Controls: React.SFC<ControlsProps> = ({ api, conditions, now, then, mgnhre
     btnClass + (canWithdraw ? 'blue' : 'buttonCTA-disabled')
 
   return (
-      <>
-        <div className="element">
-          <div className="control-wrap">
-            <h3 className="control-main">UNLOCK</h3>
-            <span className="control-desc">(entire amount only)</span>
-            <button
-              className={unlockBTNclass}
-              disabled={unlockDisabled}
-              onClick={unlockTokens}
-            >
-              UNLOCK
+    <>
+      <div className="element">
+        <div className="control-wrap">
+          <h3 className="control-main">UNLOCK</h3>
+          <span className="control-desc">(entire amount only)</span>
+          <button
+            className={unlockBTNclass}
+            disabled={unlockDisabled}
+            onClick={unlockTokens}
+          >
+            UNLOCK
             </button>
-          </div>
         </div>
-        <div className="element">
-          <p>Wait for 24 hours before it can be withdrawn</p>
-        </div>
-        <div className="element">
-          <div className="control-wrap">
-            <h3 className="control-main">WITHDRAW UNLOCKED</h3>
-            <button
-              className={withdrawBTNclass}
-              disabled={withdrawDisabled}
-              onClick={withdrawUnlockedTokens}
-            >
-              WITHDRAW
+      </div>
+      <div className="element">
+        <p className="control-info">Wait for 24 hours before it can be withdrawn</p>
+      </div>
+      <div className="element">
+        <div className="control-wrap">
+          <h3 className="control-main">WITHDRAW UNLOCKED</h3>
+          <button
+            className={withdrawBTNclass}
+            disabled={withdrawDisabled}
+            onClick={withdrawUnlockedTokens}
+          >
+            WITHDRAW
             </button>
-            <span className="control-desc">
-              {then && <Countdown now={now} then={then} />}
-            </span>
-          </div>
+          <span className="control-desc">
+            {then && <Countdown now={now} then={then} />}
+          </span>
         </div>
-        <div className="element">
+      </div>
+      <div className="element">
+        <p className="control-info">
           <p>
             Add the <a href={mgnhref} target="_blank">MGN token address</a> to your wallet. Immediately after the
-            third step you can transfer the token like any ERC20 token. Then
-            proceed to Step 5 with the other wallet address:
+            third step you can transfer the token like any ERC20 token.
           </p>
-        </div>
-        <div className="element">
-          <div className="control-wrap">
-            <h3 className="control-main">LOCK</h3>
-            <button
-              className={lockBTNclass}
-              disabled={lockDisabled}
-              onClick={lockTokens}
-            >
-              LOCK
-            </button>
-            <span className="control-desc">
-              (locks entire balance)
-            </span>
-          </div>
-        </div>
-        <p className="bottom">
-          If you want to lock less MGN than your current MGN balance, then
-          transfer the amount of MGN you do not want locked to another wallet
-          address.
+          <p>
+            Then proceed to Step 5 with the other wallet address.
+          </p>
         </p>
-      </>
+      </div>
+      <div className="element">
+        <div className="control-wrap">
+          <h3 className="control-main">LOCK</h3>
+          <button
+            className={lockBTNclass}
+            disabled={lockDisabled}
+            onClick={lockTokens}
+          >
+            LOCK
+            </button>
+          <span className="control-desc">
+            (locks entire balance)
+            </span>
+        </div>
+      </div>
+      <p className="bottom">
+        If you want to lock less MGN than your current MGN balance, then
+        transfer the amount of MGN you do not want locked to another wallet
+        address.
+        </p>
+    </>
   )
 }
 
